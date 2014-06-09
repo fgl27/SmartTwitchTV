eed3df645623ac6abea108b6ffe18437b6babf34
Nikita Katchik <vaelastratz@gmail.com>
2014-06-10 04:06:33 +0700

    Added Russian translation

---
+++ lang/ru.js
@@ -0,0 +1,20 @@
+alert("Russian (ru) loaded");
+var STR_CHANNELS = "Каналы";
+var STR_GAMES = "Игры";
+var STR_OPEN = "Открыть";
+var STR_REFRESH = "Обновить";
+
+var STR_PLACEHOLDER_OPEN = "Введите имя канала...";
+
+var STR_QUALITY = "Качество";
+
+var STR_RETRYING = "Попытка";
+var STR_VIEWER = "Зрителей";
+var STR_BUFFERING = "Буфферинг";
+
+// Not sure if errors should be translated at all
+var STR_ERROR_RENDER = "Render error.";
+var STR_ERROR_NETWORK_DISCONNECT= "Network disconnected.";
+var STR_ERROR_STREAM_NOT_FOUND = "Stream not found.";
+var STR_ERROR_AUTHENTICATION_FAIL = "Authentication failed.";
+var STR_ERROR_CONNECTION_FAIL = "Connection failed.";
