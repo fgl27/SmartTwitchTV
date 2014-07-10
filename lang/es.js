03d8021d01416276b2ad6b916c7ff5c4894e71c9
healdropper <albertoalcon@gmail.com>
2014-07-10 16:03:29 +0200

    Shortened some messages
    Messages were shortened to much the original ones to avoid visualization problems

--- lang/es.js
+++ lang/es.js
@@ -4,7 +4,7 @@ var STR_GAMES = "Juegos";
 var STR_OPEN = "Abrir";
 var STR_REFRESH = "Actualizar";
 
-var STR_PLACEHOLDER_OPEN = "Introduce un nombre de canal...";
+var STR_PLACEHOLDER_OPEN = "Introduce el nombre de un canal...";
 
 var STR_QUALITY = "Calidad";
 
@@ -12,8 +12,8 @@ var STR_RETRYING = "Reintentando";
 var STR_VIEWER = "Espectadores";
 var STR_BUFFERING = "Buffering";
 
-var STR_ERROR_RENDER = "Error durante el renderizado.";
+var STR_ERROR_RENDER = "Error de renderizado.";
 var STR_ERROR_NETWORK_DISCONNECT= "Sin red.";
-var STR_ERROR_STREAM_NOT_FOUND = "No se ha encontrado el stream";
+var STR_ERROR_STREAM_NOT_FOUND = "Stream no encontrado";
 var STR_ERROR_AUTHENTICATION_FAIL = "Fallo de autenticación.";
 var STR_ERROR_CONNECTION_FAIL = "Ha fallado la conexión.";
