34f1336c47f17ca8f4f400a0008921ac04371348
brunoradagast <brunoradagast@gmail.com>
2015-07-13 15:55:51 -0300

    Portuguese language propose.
    Please add this language to the language files.

---
+++ lang/pt-br.js
@@ -0,0 +1,21 @@
+alert("Português (pt-br) carregado");
+var STR_CHANNELS = "Canais";
+var STR_GAMES = "Jogos";
+var STR_OPEN = "Abrir";
+var STR_REFRESH = "Atualizar";
+
+var STR_PLACEHOLDER_OPEN = "Insira o nome do canal...";
+
+var STR_QUALITY = "Qualidade";
+
+var STR_RETRYING = "Tentando novamente";
+var STR_VIEWER = "Viewers";
+var STR_BUFFERING = "Buffering";
+
+
+var STR_ERROR_RENDER_SOURCE = "Formato não suportado :(";
+var STR_ERROR_RENDER_FIXED = "Desculpe mas parece que as qualidades\n High/Medium/Low não são suportadadas\npela sua TV. Tente a qualidade Source.";
+var STR_ERROR_NETWORK_DISCONNECT= "Rede desconectada.";
+var STR_ERROR_STREAM_NOT_FOUND = "Stream não encotrada.";
+var STR_ERROR_AUTHENTICATION_FAIL = "Falha na autenticação.";
+var STR_ERROR_CONNECTION_FAIL = "Falha na conexão.";
