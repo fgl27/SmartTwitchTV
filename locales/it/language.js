c4ed45b3729d09817b1fe0aa2bc65eb151ff30b4
dmanrj <pietro.galassi@gmail.com>
2016-04-06 14:23:02 +0200

    Create language.js
    italian localization

---
+++ locales/it/language.js
@@ -0,0 +1,31 @@
+console.log("Italiano (It) caricato");
+var STR_CHANNELS = "Canali";
+var STR_GAMES = "Giochi";
+var STR_OPEN = "Apri";
+var STR_REFRESH = "Aggiorna";
+
+var STR_PLACEHOLDER_OPEN = "Inserire il nome del canale....";
+
+var STR_QUALITY = "Qualita'";
+
+var STR_RETRYING = "Sto riprovando";
+var STR_VIEWER = "Spettatori";
+var STR_BUFFERING = "Caricamento in corso...";
+
+
+var STR_ERROR_RENDER_SOURCE = "Formato non supportato :(";
+var STR_ERROR_RENDER_FIXED = "Spiacenti ma sembra lo stream in \nQualita' Alta/Media/Bassa non sono supportati\n sul tuo TV. Prova a settare la qualia' sullo stream.";
+var STR_ERROR_NETWORK_DISCONNECT= "Disconnesso da internet.";
+var STR_ERROR_STREAM_NOT_FOUND = "Stream non trovato.";
+var STR_ERROR_AUTHENTICATION_FAIL = "Autenticazione fallita.";
+var STR_ERROR_CONNECTION_FAIL = "Connessione fallita.";
+
+var STR_TOOLS = "Salva";
+var STR_LIVE_CHANNELS = "Canali in diretta";
+var STR_LIVE_HOSTS = "Hosts in diretta";
+var STR_LIVE_GAMES = "Giochi in diretta";
+var STR_PLACEHOLDER_TOOLS = "Inserire nome utente...";
+var STR_CHANNEL = "Canale";
+var STR_USERNAME = "Nome utente";
+var STR_DOES_NOT_EXIST = "non esiste!";
+var STR_IS_OFFLINE = "è offline.";
