19f44bea53fb3e3a2a3991a8ed961677a2b8ca7e
Felipe Leon <fglfgl27@gmail.com>
2018-01-01 19:50:39 -0200

    Restore avplay 2
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -87,8 +87,10 @@ Play.Resume = function() {
         $("#scene_channel_panel").show();
         Play.showWarningDialog(STR_RESUME);
         Play.isReturnFromResume = true;
-        Play.onPlayer();
         Play.streamInfoTimer = window.setInterval(Play.updateStreamInfo, 60000);
+        window.setTimeout(function() {
+            Play.onPlayer();
+        }, 500);
     }
 };
 
