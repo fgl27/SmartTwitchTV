7329241c80ddfd10e3db3d90be204966a9ad7b20
Felipe Leon <fglfgl27@gmail.com>
2018-01-30 05:13:48 -0200

    Improve release_maker.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- release/release_maker.sh
+++ release/release_maker.sh
@@ -31,9 +31,9 @@ js_folders=("app/general/" "app/specific/");
 # no changes needed to be done bellow this line
 
 if ! which 'sed' >/dev/null  ; then
-	echo -e "\ncan't run sed it's not installed";
+	echo -e "\\ncan't run sed it's not installed";
         echo -e "Install using command:";
-        echo -e "sudo apt-get install sed\n";
+        echo -e "sudo apt-get install sed\\n";
 	echo -e "Release maker aborted"
 	exit;
 fi;
@@ -42,69 +42,69 @@ sh_folder="$(dirname "$0")";
 mainfolder="$(dirname "$sh_folder")";
 canuglifyjs=0;
 
-cd $mainfolder
+cd "$mainfolder" || exit
 sed_comp() {
         array=( "$@" );
-	for i in ${array[@]}; do
+	for i in "${array[@]}"; do
 		echo -e "Compressing $i";
-                sed -i -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' $i;
-                sed -i "/\/\*.*\*\//d;/\/\*/,/\*\// d" $i;
-		sed -i '/^\(\s*\)\/\//d' $i;
-		sed -i 's/^[ \t]*//g; s/[ \t]*$//g' $i;
-		sed -i ':a;N;$!ba;s/\n/ /g' $i;
-		sed -i 's/\s*:/:/g' $i;
-		sed -i 's/; \+/;/g' $i;
-		sed -i 's/: \+/:/g' $i;
-		sed -i 's/> \+/>/g' $i;
-		sed -i 's/\s*</</g' $i;
-		sed -i 's/} \+/}/g' $i;
-		sed -i 's/{ \+/{/g' $i;
-		sed -i 's/\s*{/{/g' $i;
-		sed -i 's/\s*}/}/g' $i;
-#		sed -i 's/" \+/"/g' $i;
+                sed -i -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' "$i";
+                sed -i "/\\/\\*.*\\*\\//d;/\\/\\*/,/\\*\\// d" "$i";
+		sed -i '/^\(\s*\)\/\//d' "$i";
+		sed -i 's/^[ \t]*//g; s/[ \t]*$//g' "$i";
+		sed -i ':a;N;$!ba;s/\n/ /g' "$i";
+		sed -i 's/\s*:/:/g' "$i";
+		sed -i 's/; \+/;/g' "$i";
+		sed -i 's/: \+/:/g' "$i";
+		sed -i 's/> \+/>/g' "$i";
+		sed -i 's/\s*</</g' "$i";
+		sed -i 's/} \+/}/g' "$i";
+		sed -i 's/{ \+/{/g' "$i";
+		sed -i 's/\s*{/{/g' "$i";
+		sed -i 's/\s*}/}/g' "$i";
+#		sed -i 's/" \+/"/g' "$i";
 	done
 }
 
 js_comp_yuo() {
         array=( "$@" );
-	for i in ${array[@]}; do
-		cd $i || exit;
+	for i in "${array[@]}"; do
+		cd "$i" || exit;
 		for x in *.js; do
 			echo -e "Compressing $x";
 			yui-compressor "$x" -o "$x";
 		done
-		cd - > /dev/null;
+		cd - &> /dev/null || exit;
 	done
 }
 
 js_comp_ugf() {
         array=( "$@" );
-	for i in ${array[@]}; do
-		cd $i || exit;
+	for i in "${array[@]}"; do
+		cd "$i" || exit;
 		for x in *.js; do
 			echo -e "Compressing $x";
 			uglifyjs "$x" -c -m -o "$x";
 		done
-		cd - > /dev/null;
+		cd - &> /dev/null || exit;
 	done
 }
 
 master_maker() {
         array=( "$@" );
-	for i in ${array[@]}; do
-		cd $i || exit;
+	for i in "${array[@]}"; do
+		cd "$i" || exit;
 		for x in *.js; do
 			echo -e "Including $x";
-			echo $(cat $x) >> $mainfolder/release/master.js;
+			cat "$x" >> "$mainfolder"/release/master.js;
 		done
-		cd - > /dev/null;
+		cd - &> /dev/null || exit;
 	done
 }
 
-echo -e "\nStarting Release maker\n";
+echo -e "\\nStarting Release maker\\n";
 
 sed -i 's/isReleased = false/isReleased = true/g' app/specific/Main.js;
-echo "$(cat index.html | sed -n '/bodystart/,/bodyend/p')" > index.html
+sed -i -n '/bodystart/,/bodyend/p' index.html
 rm -rf release/app/
 mkdir -p 'release/app/images/'
 cp -rf app/images/app_icon.png release/app/images/app_icon.png
@@ -112,7 +112,7 @@ cp -rf widget.info release/widget.info
 cp -rf .project release/.project
 cp -rf .tproject release/.tproject
 
-echo -e "\nCompressing Start\n";
+echo -e "\\nCompressing Start\\n";
 
 sed_comp "${html_file[@]}";
 
@@ -122,39 +122,39 @@ if which 'uglifyjs' >/dev/null  ; then
 elif which 'yui-compressor' >/dev/null  ; then
 	js_comp_yuo "${js_folders[@]}";
 else
-	echo -e "\ncan't run uglifyjs or yui-compressor as they are not installed";
-        echo -e "To install uglifyjs read the release maker notes on the top\n";
+	echo -e "\\ncan't run uglifyjs or yui-compressor as they are not installed";
+        echo -e "To install uglifyjs read the release maker notes on the top\\n";
         echo -e "Install yui-compressor using command:";
-        echo -e "sudo apt-get install yui-compressor\n";
-	echo -e ".js files not compressed.\n"
+        echo -e "sudo apt-get install yui-compressor\\n";
+	echo -e ".js files not compressed.\\n"
 fi;
 
-echo -e "\nCompression done\n";
+echo -e "\\nCompression done\\n";
 
-echo -e "\nMaking new files up\n";
+echo -e "\\nMaking new files up\\n";
 
 cp -rf config.xml release/config.xml
-echo "var STR_BODY='"$(cat index.html)"';" > release/master.js;
+echo "var STR_BODY='""$(cat index.html)""';" > release/master.js;
 master_maker "${js_folders[@]}"
 
-cd release/
+cd release/ || exit
 rm -rf release.zip
 zip -qr9 release ./ -x master.* html_body.js master.js release_maker.sh \*githubio\*
 git stash &> /dev/null
 
 if [ "$canuglifyjs" == 1 ]; then
-	uglifyjs master.js -c -o master.js
+	uglifyjs master.js -c -o master.js;
 fi;
 
-echo -e "\nMaking done\n";
+echo -e "\\nMaking done\\n";
 
-echo -e "Release zip generated at $mainfolder/release/release.zip\n";
-cp -rf master.js githubio/js/master.js
-cd - &> /dev/null
+echo -e "Release zip generated at $mainfolder/release/release.zip\\n";
+cp -rf master.js githubio/js/master.js;
+cd - &> /dev/null || exit;
 git_check="$(git status | grep modified)";
 
 if [ ! -z "$git_check" ]; then
-	echo -e "Is necessary to update githubio as below files are modify:\n"
+	echo -e "Is necessary to update githubio as below files are modify:\\n"
 	echo -e "$git_check"
 fi;
 exit;
