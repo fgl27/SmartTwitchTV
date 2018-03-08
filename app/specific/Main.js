ee64a7a9895ba9f082af30736ff1ea84ff583edc
Felipe Leon <fglfgl27@gmail.com>
2018-03-08 19:20:15 -0300

    Up release/githubio/css/master.css and js/master.js
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -108,7 +108,7 @@ var TEMP_MP4 = "https://bhb27.github.io/smarttv-twitch/release/githubio/images/t
 Main.version = 400;
 Main.stringVersion = '4.0.0';
 Main.currentVersion = '';
-Main.minversion = '030618';
+Main.minversion = '030818';
 Main.versonTag = '';
 //Variable initialization end
 
