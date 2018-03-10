44baa984d7be8c068ca8cd9c0711f79daea0c98c
Felipe Leon <fglfgl27@gmail.com>
2018-03-10 17:37:40 -0300

    AGame: add latest improves to loading and replacing  + etc improves
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/AGame.js
+++ app/specific/AGame.js
@@ -7,11 +7,9 @@ AGame.cursorY = 0;
 AGame.cursorX = 0;
 AGame.dataEnded = false;
 AGame.itemsCount = 0;
-AGame.imgMatrix = [];
-AGame.imgMatrixId = [];
-AGame.imgMatrixCount = 0;
 AGame.nameMatrix = [];
 AGame.nameMatrixCount = 0;
+AGame.blankCellVector = [];
 AGame.loadingData = false;
 AGame.loadingDataTry = 0;
 AGame.loadingDataTryMax = 10;
@@ -33,6 +31,7 @@ AGame.ViwersDiv = 'agame_viwers_';
 AGame.QualityDiv = 'agame_quality_';
 AGame.Cell = 'agame_cell_';
 AGame.status = false;
+AGame.itemsCountCheck = false;
 
 //Variable initialization end
 
@@ -72,6 +71,8 @@ AGame.StartLoad = function() {
     AGame.MaxOffset = 0;
     AGame.nameMatrix = [];
     AGame.nameMatrixCount = 0;
+    AGame.blankCellVector = [];
+    AGame.itemsCountCheck = false;
     AGame.itemsCount = 0;
     AGame.cursorX = 0;
     AGame.cursorY = 0;
@@ -82,9 +83,7 @@ AGame.StartLoad = function() {
 };
 
 AGame.loadDataPrepare = function() {
-    AGame.imgMatrix = [];
-    AGame.imgMatrixId = [];
-    AGame.imgMatrixCount = 0;
+    Main.MatrixRst();
     AGame.loadingData = true;
     AGame.loadingDataTry = 0;
     AGame.loadingDataTimeout = 3500;
@@ -175,7 +174,12 @@ AGame.loadDataSuccess = function(responseText) {
         }
 
         for (coloumn_id; coloumn_id < Main.ColoumnsCountVideo; coloumn_id++) {
+            if (AGame.dataEnded && !AGame.itemsCountCheck) {
+                AGame.itemsCountCheck = true;
+                AGame.itemsCount = (row_id * Main.ColoumnsCountVideo) + coloumn_id;
+            }
             row.append(Main.createCellEmpty(row_id, coloumn_id, AGame.EmptyCell));
+            AGame.blankCellVector.push(AGame.EmptyCell + row_id + '_' + coloumn_id);
         }
         $('#' + Main.TempTable).append(row);
     }
@@ -183,28 +187,28 @@ AGame.loadDataSuccess = function(responseText) {
 };
 
 AGame.createCell = function(row_id, coloumn_id, channel_name, preview_thumbnail, stream_title, stream_game, channel_display_name, viwers, quality) {
-    preview_thumbnail = preview_thumbnail.replace("{width}x{height}", Main.VideoSize);
+    AGame.CellMatrix(channel_name, preview_thumbnail, row_id, coloumn_id);
+    return $('<td id="' + AGame.Cell + row_id + '_' + coloumn_id + '" class="stream_cell" data-channelname="' + channel_name + '"></td>').html(
+        AGame.CellHtml(row_id, coloumn_id, channel_display_name, stream_title, stream_game, viwers, quality));
+};
 
-    AGame.imgMatrix[AGame.imgMatrixCount] = preview_thumbnail;
-    AGame.imgMatrixId[AGame.imgMatrixCount] = AGame.Thumbnail + row_id + '_' + coloumn_id;
-    AGame.imgMatrixCount++;
-
-    if (AGame.imgMatrixCount < (Main.ColoumnsCountVideo * 5)) Main.PreLoadAImage(preview_thumbnail); //try to pre cache first 4 rows
-
+AGame.CellMatrix = function(channel_name, preview_thumbnail, row_id, coloumn_id) {
+    Main.CellMatrix(preview_thumbnail, Main.ColoumnsCountVideo, AGame.Thumbnail, row_id, coloumn_id, Main.VideoSize);
     AGame.nameMatrix[AGame.nameMatrixCount] = channel_name;
     AGame.nameMatrixCount++;
+};
 
-    return $('<td id="' + AGame.Cell + row_id + '_' + coloumn_id + '" class="stream_cell" data-channelname="' + channel_name + '"></td>').html(
-        '<img id="' + AGame.Thumbnail + row_id + '_' + coloumn_id + '" class="stream_thumbnail" src="' + IMG_LOD_VIDEO + '"/>' +
+AGame.CellHtml = function(row_id, coloumn_id, channel_display_name, stream_title, stream_game, viwers, quality) {
+    return '<img id="' + AGame.Thumbnail + row_id + '_' + coloumn_id + '" class="stream_thumbnail" src="' + IMG_LOD_VIDEO + '"/>' +
         '<div id="' + AGame.ThumbnailDiv + row_id + '_' + coloumn_id + '" class="stream_text">' +
         '<div id="' + AGame.DispNameDiv + row_id + '_' + coloumn_id + '" class="stream_channel">' + channel_display_name + '</div>' +
         '<div id="' + AGame.StreamTitleDiv + row_id + '_' + coloumn_id + '"class="stream_info">' + stream_title + '</div>' +
         '<div id="' + AGame.StreamGameDiv + row_id + '_' + coloumn_id + '"class="stream_info">' + stream_game + '</div>' +
-        '<div id="' + AGame.ViwersDiv + row_id + '_' + coloumn_id + '"class="stream_info_games" style="width: 40%; display: inline-block;">' + viwers +
-        '</div>' +
+        '<div id="' + AGame.ViwersDiv + row_id + '_' + coloumn_id + '"class="stream_info" style="width: 40%; display: inline-block;">' + viwers + '</div>' +
         '<div id="' + AGame.QualityDiv + row_id + '_' + coloumn_id +
-        '"class="stream_info" style="width:35%; float: right; display: inline-block;">' + quality + '</div></div>');
+        '"class="stream_info" style="width:35%; float: right; display: inline-block;">' + quality + '</div></div>';
 };
+
 
 AGame.CellExists = function(display_name) {
     for (var i = 0; i <= AGame.nameMatrixCount; i++) {
@@ -231,12 +235,12 @@ AGame.loadDataSuccessFinish = function() {
 
                 Main.HideLoadDialog();
                 AGame.addFocus();
-                Main.LoadImages(AGame.imgMatrix, AGame.imgMatrixId, IMG_404_VIDEO);
+                Main.LoadImagesPre(IMG_404_VIDEO);
 
                 AGame.loadingData = false;
             } else {
                 Main.AddTable('stream_table_a_game');
-                Main.LoadImages(AGame.imgMatrix, AGame.imgMatrixId, IMG_404_VIDEO);
+                Main.LoadImagesPre(IMG_404_VIDEO);
 
                 if (AGame.blankCellCount > 0 && !AGame.dataEnded) {
                     AGame.loadingMore = true;
@@ -290,74 +294,65 @@ AGame.loadDataErrorReplace = function() {
     if (AGame.loadingDataTry < AGame.loadingDataTryMax) {
         AGame.loadingDataTimeout += (AGame.loadingDataTry < 5) ? 250 : 3500;
         AGame.loadDataReplace();
+    } else {
+        AGame.ReplacedataEnded = true;
+        AGame.blankCellCount = 0;
+        AGame.blankCellVector = [];
+        AGame.loadDataSuccessFinish();
     }
 };
 
 AGame.loadDataSuccessReplace = function(responseText) {
     var response = $.parseJSON(responseText);
     var response_items = response.streams.length;
+    var stream, index, cursor = 0;
+    var tempVector = AGame.blankCellVector.slice();
+
     AGame.MaxOffset = parseInt(response._total);
 
     if (response_items < Main.ItemsLimitVideo) AGame.ReplacedataEnded = true;
 
-    var row_id = AGame.itemsCount / Main.ColoumnsCountVideo;
 
-    var coloumn_id, stream, mReplace = false,
-        cursor = 0;
-
-    for (cursor; cursor < response_items; cursor++) {
+    for (var i = 0; i < AGame.blankCellVector.length && cursor < response_items; i++, cursor++) {
         stream = response.streams[cursor];
-        if (AGame.CellExists(stream.channel.name)) AGame.blankCellCount--;
-        else {
-            mReplace = AGame.replaceCellEmpty(row_id, coloumn_id, stream.channel.name, stream.preview.template,
+        if (AGame.CellExists(stream.channel.name)) {
+            AGame.blankCellCount--;
+            i--;
+        } else {
+            AGame.replaceCellEmpty(AGame.blankCellVector[i], stream.channel.name, stream.preview.template,
                 stream.channel.status, stream.game, Main.is_playlist(JSON.stringify(stream.stream_type)) +
                 stream.channel.display_name, Main.addCommas(stream.viewers) + STR_VIEWER,
                 Main.videoqualitylang(stream.video_height, stream.average_fps, stream.channel.language));
-            if (mReplace) AGame.blankCellCount--;
-            if (AGame.blankCellCount === 0) break;
-        }
-    }
-    AGame.itemsCountOffset += cursor;
-    if (AGame.ReplacedataEnded) AGame.blankCellCount = 0;
-    AGame.loadDataSuccessFinish();
-};
+            AGame.blankCellCount--;
 
-AGame.replaceCellEmpty = function(row_id, coloumn_id, channel_name, preview_thumbnail, stream_title, stream_game, channel_display_name, viwers, quality) {
-    var my = 0,
-        mx = 0;
-    if (row_id < ((Main.ItemsLimitVideo / Main.ColoumnsCountVideo) - 1)) return false;
-    for (my = row_id - (1 + Math.ceil(AGame.blankCellCount / Main.ColoumnsCountVideo)); my < row_id; my++) {
-        for (mx = 0; mx < Main.ColoumnsCountVideo; mx++) {
-            if (!Main.ThumbNull(my, mx, AGame.Thumbnail) && (Main.ThumbNull(my, mx, AGame.EmptyCell))) {
-                row_id = my;
-                coloumn_id = mx;
-                preview_thumbnail = preview_thumbnail.replace("{width}x{height}", Main.VideoSize);
-
-                AGame.imgMatrix[AGame.imgMatrixCount] = preview_thumbnail;
-                AGame.imgMatrixId[AGame.imgMatrixCount] = AGame.Thumbnail + row_id + '_' + coloumn_id;
-                AGame.imgMatrixCount++;
-
-                AGame.nameMatrix[AGame.nameMatrixCount] = channel_name;
-                AGame.nameMatrixCount++;
-                document.getElementById(AGame.EmptyCell + row_id + '_' + coloumn_id).setAttribute('id', AGame.Cell + row_id + '_' + coloumn_id);
-                document.getElementById(AGame.Cell + row_id + '_' + coloumn_id).setAttribute('data-channelname', channel_name);
-                document.getElementById(AGame.Cell + row_id + '_' + coloumn_id).innerHTML =
-                    '<img id="' + AGame.Thumbnail + row_id + '_' + coloumn_id + '" class="stream_thumbnail" src="' + IMG_LOD_VIDEO + '"/>' +
-                    '<div id="' + AGame.ThumbnailDiv + row_id + '_' + coloumn_id + '" class="stream_text">' +
-                    '<div id="' + AGame.DispNameDiv + row_id + '_' + coloumn_id + '" class="stream_channel">' + channel_display_name + '</div>' +
-                    '<div id="' + AGame.StreamTitleDiv + row_id + '_' + coloumn_id + '"class="stream_info">' + stream_title + '</div>' +
-                    '<div id="' + AGame.StreamGameDiv + row_id + '_' + coloumn_id + '"class="stream_info">' + stream_game + '</div>' +
-                    '<div id="' + AGame.ViwersDiv + row_id + '_' + coloumn_id +
-                    '"class="stream_info_games" style="width: 40%; display: inline-block;">' + viwers +
-                    '</div>' +
-                    '<div id="' + AGame.QualityDiv + row_id + '_' + coloumn_id +
-                    '"class="stream_info" style="width:35%; float: right; display: inline-block;">' + quality + '</div></div>';
-                return true;
+            index = tempVector.indexOf(tempVector[i]);
+            if (index > -1) {
+                tempVector.splice(index, 1);
             }
         }
     }
 
-    return false;
+    AGame.itemsCountOffset += cursor;
+    if (AGame.ReplacedataEnded) {
+        AGame.blankCellCount = 0;
+        AGame.blankCellVector = [];
+    } else AGame.blankCellVector = tempVector;
+
+    AGame.loadDataSuccessFinish();
+};
+
+AGame.replaceCellEmpty = function(id, channel_name, preview_thumbnail, stream_title, stream_game, channel_display_name, viwers, quality) {
+    var splitedId = id.split("_");
+    var row_id = splitedId[1];
+    var coloumn_id = splitedId[2];
+    var cell = AGame.Cell + row_id + '_' + coloumn_id;
+
+    AGame.CellMatrix(channel_name, preview_thumbnail, row_id, coloumn_id);
+
+    document.getElementById(id).setAttribute('id', cell);
+    document.getElementById(cell).setAttribute('data-channelname', channel_name);
+    document.getElementById(cell).innerHTML =
+        AGame.CellHtml(row_id, coloumn_id, channel_display_name, stream_title, stream_game, viwers, quality);
 };
 
 AGame.addFocus = function() {
