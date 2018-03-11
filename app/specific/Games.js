25a31d6386f7cc53c11d0442b512fc4280c91e66
Felipe Leon <fglfgl27@gmail.com>
2018-03-11 19:12:27 -0300

    Main: ReplaceTable use append for better compatibility
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Games.js
+++ app/specific/Games.js
@@ -225,7 +225,7 @@ Games.loadDataSuccessFinish = function() {
             if (!Games.Status) {
                 Games.Status = true;
 
-                Main.ReplaceTable('stream_table_games');
+                Main.appendTable('stream_table_games');
 
                 Main.HideLoadDialog();
                 Games.addFocus();
@@ -233,7 +233,7 @@ Games.loadDataSuccessFinish = function() {
 
                 Games.loadingData = false;
             } else {
-                Main.AddTable('stream_table_games');
+                Main.appendTable('stream_table_games');
                 Main.LoadImagesPre(IMG_404_GAME);
 
                 if (Games.blankCellCount > 0 && !Games.dataEnded) {
--- app/specific/Live.js
+++ app/specific/Live.js
@@ -229,7 +229,7 @@ Live.loadDataSuccessFinish = function() {
             if (!Live.Status) {
                 Live.Status = true;
 
-                Main.ReplaceTable('stream_table_live');
+                Main.appendTable('stream_table_live');
 
                 Main.HideLoadDialog();
                 Live.addFocus();
@@ -243,7 +243,7 @@ Live.loadDataSuccessFinish = function() {
                     if (Main.checkVersion()) Main.showUpdateDialog();
                 }
             } else {
-                Main.AddTable('stream_table_live');
+                Main.appendTable('stream_table_live');
                 Main.LoadImagesPre(IMG_404_VIDEO);
 
                 if (Live.blankCellCount > 0 && !Live.dataEnded) {
