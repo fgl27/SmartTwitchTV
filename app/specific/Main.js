d639bed7ff3f1ede4bf12b5f923c7490acc8a085
Felipe Leon <fglfgl27@gmail.com>
2018-03-29 15:02:18 -0300

    Up master files
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -123,7 +123,7 @@ var IMG_SMART_USER = GIT_IO + "smart_add_user.png";
 Main.version = 400;
 Main.stringVersion = '4.0.0';
 Main.currentVersion = '';
-Main.minversion = '032818';
+Main.minversion = '032918';
 Main.versonTag = '';
 //Variable initialization end
 
