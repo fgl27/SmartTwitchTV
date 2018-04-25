58185cf4dfae5d4d470ef50d0086c4a842ed397e
Felipe Leon <fglfgl27@gmail.com>
2018-04-25 15:33:41 -0300

    Add language to players info pannel
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Sclip.js
+++ app/specific/Sclip.js
@@ -175,7 +175,7 @@ function Sclip_loadDataSuccess(responseText) {
                 row.appendChild(Sclip_createCell(row_id, row_id + '_' + coloumn_id,
                     video.thumbnails.medium.split('-preview')[0] + '.mp4' + video.duration, [video.thumbnails.medium,
                         video.title, video.game, STR_STREAM_ON + Main_videoCreatedAt(video.created_at),
-                        Main_addCommas(video.views) + STR_VIEWER, STR_DURATION + Play_timeS(video.duration)
+                        Main_addCommas(video.views) + STR_VIEWS, STR_DURATION + Play_timeS(video.duration)
                     ]));
             }
         }
