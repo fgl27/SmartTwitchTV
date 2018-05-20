36e3ed114a6f756e49f85762b995f0e1c244cf30
Felipe Leon <fglfgl27@gmail.com>
2018-05-20 14:22:38 -0300

    Update release/release_maker.sh
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- release/release_maker.sh
+++ release/release_maker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#code compressor using uglifyjs, sed and cleancss, runs on linux shell base system
+#code compressor using uglifyjs, jshint, js-beautify, sed and cleancss, this .sh runs on linux shell base system
 
-#instalation of uglifyjs has more then one step
+#instalation of uglifyjs and jshint has more then one step
 #1# Donwload npm/node and https://nodejs.org/en/
 # extract the download file then do the bellow on the terminal
 
@@ -13,7 +13,7 @@
 # export PATH=$NODEJS_HOME/bin:$PATH
 
 # now install uglifyjs via terminal
-# npm install uglify-js -g
+# npm install uglify-js jshint js-beautify -g
 
 #installation of sed and cleancss is via more used apt-get
 
@@ -24,7 +24,8 @@
 #exec this file or drag this .sh file to terminal to generate a released
 
 # add html files here, master.css here is a temp file generate by this .sh it has the css content of index.html
-html_file=("config.xml" "index.html" "master.css" "release/index.html");
+temp_maker_folder="release/temp_maker/";
+html_file=("$temp_maker_folder""config.xml" "$temp_maker_folder""index.html" "$temp_maker_folder""master.css" "release/index.html");
 
 # add js folders here
 js_folders=("app/languages/" "app/general/" "app/specific/");
@@ -41,6 +42,30 @@ if ! which 'sed' >/dev/null  ; then
 fi;
 
 # Exit if uglifyjs is not available
+canjshint=0;
+if which 'jshint' >/dev/null  ; then
+	# call this .sh and 1 "this.sh 1" to update uglify-js
+	if [ "$1" == 1 ]; then
+		npm install jshint -g
+	fi;
+	canjshint=1;
+else
+	echo -e "\\ncan't run jshint, as it's not installed";
+	echo -e "To install jshint read the release maker notes on the top\\n";
+	echo -e "Release maker aborted\\n"
+	exit;
+fi;
+
+canbeautify=0;
+if which 'js-beautify' >/dev/null  ; then
+	# call this .sh and 1 "this.sh 1" to update uglify-js
+	if [ "$1" == 1 ]; then
+		npm install js-beautify -g
+	fi;
+	canbeautify=1;
+fi;
+
+# Exit if uglifyjs is not available
 canuglifyjs=0;
 if which 'uglifyjs' >/dev/null  ; then
 	# call this .sh and 1 "this.sh 1" to update uglify-js
@@ -49,9 +74,9 @@ if which 'uglifyjs' >/dev/null  ; then
 	fi;
 	canuglifyjs=1;
 else
-	echo -e "\\ncan't run uglifyjs or yui as it is not installed";
+	echo -e "\\ncan't run uglifyjs, as it's not installed";
 	echo -e "To install uglifyjs read the release maker notes on the top\\n";
-	echo -e ".js files not compressed.\\n"
+	echo -e "Release maker aborted\\n"
 	exit;
 fi;
 
@@ -74,7 +99,7 @@ cd "$mainfolder" || exit
 sed_comp() {
 	array=( "$@" );
 	for i in "${array[@]}"; do
-		echo -e "sed compressing $i";
+		echo -e "	sed compressing $i";
 		sed -i -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' "$i";
 		sed -i "/\\/\\*.*\\*\\//d;/\\/\\*/,/\\*\\// d" "$i";
 		sed -i '/^\(\s*\)\/\//d' "$i";
@@ -100,21 +125,68 @@ js_comp_ugf() {
 	for i in "${array[@]}"; do
 		cd "$i" || exit;
 		for x in *.js; do
-			echo -e "Including compresed version of $x to master.js";
-			uglifyjs "$x" -c -m -o "$x";
-			cat "$x" >> "$mainfolder"/release/master.js;
+			echo -e "	Including compresed version of $x to master.js";
+			uglifyjs "$x" -c -m -o "$mainfolder"/"$temp_maker_folder""$x";
+			cat "$mainfolder"/"$temp_maker_folder""$x" >> "$mainfolder"/release/master.js;
 		done
 		cd - &> /dev/null || exit;
 	done
 }
 
-echo -e "\\nStarting Release maker";
+js_jshint() {
+        array=( "$@" );
+	for i in "${array[@]}"; do
+		cd "$i" || exit;
+		for x in *.js; do
+			cat "$x" >> "$mainfolder"/release/master.js;
+		done
+		cd - &> /dev/null || exit;
+	done
+
+	jsh_check="$(jshint "$mainfolder"/release/master.js)";
+	if [ ! -z "$jsh_check" ]; then
+		echo -e "	JSHint erros or warnings found:\\n"
+		echo -e "	$jsh_check"
+		echo -e "\\n	Fix the problems and try the release maker again\\n"
+		exit;
+	else
+		echo -e "	JSHint Test finished no errors or warnings found"
+	fi;
+}
+
+js_beautify() {
+	js-beautify index.html -o index.html;
+	array=( "$@" );
+	for i in "${array[@]}"; do
+		cd "$i" || exit;
+		for x in *.js; do
+			js-beautify "$x" -o "$x";
+		done
+		cd - &> /dev/null || exit;
+	done
+}
+
+echo -e "\\nStarting Release maker\\n";
+
+if [ "$canjshint" == 1 ]; then
+	echo -e "JSHint Test started...\\n";
+	echo -e '/* jshint undef: true, unused: true, node: true, browser: true */\n/*globals tizen, webapis, escape, STR_BODY */' > "$mainfolder"/release/master.js;
+	js_jshint "${js_folders[@]}";
+fi;
 
 # Prepare the release folder copy files to correct place and make new temp files
 sed -i 's/Main_isReleased = false/Main_isReleased = true/g' app/specific/Main.js;
-cp -rf index.html master.css
-sed -i -n '/bodystart/,/bodyend/p' index.html
-sed -i -n '/cssstart/,/cssend/p' master.css
+
+mkdir -p "$temp_maker_folder"
+
+cp -rf index.html "$temp_maker_folder"master.css
+cp -rf index.html "$temp_maker_folder"index.html
+cp -rf config.xml "$temp_maker_folder"config.xml
+cp -rf release/index.html index_release.html
+
+sed -i -n '/bodystart/,/bodyend/p' "$temp_maker_folder"index.html
+sed -i -n '/cssstart/,/cssend/p' "$temp_maker_folder"master.css
+
 rm -rf release/app/
 mkdir -p 'release/app/images/'
 cp -rf app/images/app_icon.png release/app/images/app_icon.png
@@ -128,8 +200,8 @@ echo -e "\\nCompressing Start\\n";
 sed_comp "${html_file[@]}";
 
 # Include STR_BODY to release/master, STR_BODY has the content of index.html body
-cp -rf config.xml release/config.xml
-echo "var STR_BODY='""$(cat index.html)""';" > release/master.js;
+cp -rf "$temp_maker_folder"config.xml release/config.xml
+echo "var STR_BODY='""$(cat "$temp_maker_folder"index.html)""';" > release/master.js;
 
 if [ "$canuglifyjs" == 1 ]; then
 	js_comp_ugf "${js_folders[@]}";
@@ -138,7 +210,7 @@ fi;
 #Make a zip
 cd release/ || exit
 rm -rf *.zip
-zip -qr9 release ./ -x master.* html_body.js master.js release_maker.sh beautify.sh jshint.sh \*githubio\*
+zip -qr9 release ./ -x master.* html_body.js master.js release_maker.sh beautify.sh jshint.sh \*githubio\* \*temp_maker\*
 
 # Clean up release/ folder temp files and stash all over git changes
 rm -rf app/
@@ -148,37 +220,54 @@ rm -rf .project release/.project
 rm -rf .tproject release/.tproject
 
 cd - &> /dev/null || exit;
-git stash &> /dev/null;
 
 # Compress using cleancss
 if [ "$cancleancss" == 1 ]; then
-	cleancss master.css -o master.css
+	cleancss "$temp_maker_folder"master.css -o "$temp_maker_folder"master.css
 	cleancss release/githubio/css/font-awesome.css -o release/githubio/css/font-awesome.min.css
 fi;
 # Copy master.css to its place, it's the css content of index.html
-cp -rf master.css release/githubio/css/master.css
-rm -rf master.css;
+cp -rf "$temp_maker_folder"master.css release/githubio/css/master.css
+
+cp -rf index_release.html release/index.html 
+rm -rf index_release.html
 cd release/ || exit
 
 # Run uglifyjs one more time with "toplevel" enable, only here as if run before js files don't work, the result is around 10% compression improve
 if [ "$canuglifyjs" == 1 ]; then
-	echo -e "uglifyjs master.js";
+	echo -e "	uglifyjs master.js";
 	uglifyjs master.js -c -m toplevel -o master.js;
 fi;
 
-echo -e "Compression done\\n";
-
-echo -e "Release done, zip generated at $mainfolder/release/release.zip\\n";
+echo -e "\\nCompression done\\n";
 
 # copy master.js temp files to githubio/js/
 cp -rf master.js githubio/js/master.js;
 cd - &> /dev/null || exit;
+rm -rf "$temp_maker_folder"
+sed -i 's/Main_isReleased = true/Main_isReleased = false/g' app/specific/Main.js;
+
+if [ "$canbeautify" == 1 ]; then
+	echo -e "JS Beautifier code formarter started...\\n";
+	beautify_check="$(js_beautify "${js_folders[@]}" | grep -v unchanged)";
+	if [ ! -z "$beautify_check" ]; then
+		echo -e "	JS Beautifier - finished below files are beautified:\\n"
+		echo -e "	$beautify_check"
+	else
+		echo -e "	JS Beautifier - finished none file modify\\n"
+	fi;
+else
+	echo -e "\\ncan't run js-beautify, as it's not installed";
+	echo -e "To install js-beautify read the release maker notes on the top\\n";
+	echo -e "Repo files not beautifyed\\n"
+fi;
 
 # Warn if a change was detected to master.js or master.css file
 git_check="$(git status | grep modified)";
 if [ ! -z "$git_check" ]; then
-	echo -e "Is necessary to update githubio as below files are modify:\\n"
+	echo -e "\\nIs necessary to update githubio as below files are modify:\\n"
 	echo -e "$git_check"
 fi;
 
+echo -e "\\nRelease done, zip generated at $mainfolder/release/release.zip\\n";
 exit;
