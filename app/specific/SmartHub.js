e06dee84401569baab65a9dff804372aadb97db5
Felipe Leon <fglfgl27@gmail.com>
2018-06-20 23:23:42 -0300

    SmartHub reformat preview generator
    More readable and reliable functions
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/SmartHub.js
+++ app/specific/SmartHub.js
@@ -13,6 +13,9 @@ var SmartHub_emptyUser = false;
 var SmartHub_SmartHubResume = false;
 var SmartHub_followerUsername = '';
 var SmartHub_previewData = 0;
+
+var SmartHub_BasePreviewData;
+var SmartHub_BasePreviewDataNoUser;
 //Variable initialization end
 
 function SmartHub_Start() {
@@ -28,7 +31,7 @@ function SmartHub_Start() {
     SmartHub_previewData = 0;
     if (SmartHub_emptyUser) {
         try {
-            webapis.preview.setPreviewData(previewDataGeneratorEmpty());
+            webapis.preview.setPreviewData(JSON.stringify(SmartHub_BasePreviewDataNoUser));
         } catch (ex) {
             console.log(ex.message);
         }
@@ -56,7 +59,7 @@ function SmartHub_loadDataRequest() {
             theUrl = 'https://api.twitch.tv/kraken/users/' + encodeURIComponent(SmartHub_followerUsername) +
                 '/follows/channels?limit=100&sortby=last_broadcast&' + Math.round(Math.random() * 1e7);
         } else if (SmartHub_previewData === 1) {
-            theUrl = 'https://api.twitch.tv/kraken/streams/?channel=' + encodeURIComponent(SmartHub_followerChannels) + '&limit=21&' +
+            theUrl = 'https://api.twitch.tv/kraken/streams/?channel=' + encodeURIComponent(SmartHub_followerChannels) + '&limit=18&' +
                 Math.round(Math.random() * 1e7);
         } else if (SmartHub_previewData === 2) { // user games
             theUrl = 'https://api.twitch.tv/api/users/' + encodeURIComponent(SmartHub_followerUsername) + '/follows/games/live?limit=6&' +
@@ -118,17 +121,17 @@ function SmartHub_previewDataSuccess(responseText) {
         SmartHub_loadDataRequest();
     } else {
         SmartHub_LastUpdate = new Date().getTime();
-        msetPreviewData();
+        SmartHub_msetPreviewData();
     }
 }
 
-function msetPreviewData() {
+function SmartHub_msetPreviewData() {
     //first we erase all data '{}' to make shore when the new data is load it updates the imgs
-    //then set a new data previewDataGenerator(), on sucess we SmartHub_cleanVector
+    //then set a new data SmartHub_previewDataGenerator(), on sucess we SmartHub_cleanVector
     try {
         webapis.preview.setPreviewData('{}', function() {
             try {
-                webapis.preview.setPreviewData(previewDataGenerator(), function() {
+                webapis.preview.setPreviewData(SmartHub_previewDataGenerator(), function() {
                     window.setTimeout(SmartHub_cleanVector, 1000);
                 });
             } catch (ex) {
@@ -145,99 +148,84 @@ function SmartHub_StartInterval() {
     Main_SmartHubId = window.setInterval(SmartHub_Start, 600000);
 }
 
-function previewDataGeneratorEmpty() {
-    return '{"sections":[' + previewDataGeneratorEnd(true);
-}
+function SmartHub_previewDataGenerator() {
+    var i, vectorSize, Title, tile;
+    //Re-initialize SmartHub_BasePreviewData because it may not be yet or some section have be removed
+    SmartHub_SetBasePreviewData();
 
-function previewDataGenerator() {
-    var data = '{"sections":[',
-        i, vectorSize, LiveTitle, HostTitle;
-
-    // new lines separates it blocks to make easier to add new blocks, just copy/paste a block
-
+    //Live
     vectorSize = SmartHub_userlive.length;
     if (vectorSize) {
-        data += '{"title":"' + STR_LIVE_CHANNELS + ' ' + SmartHub_followerUsername + '","tiles":[';
+        SmartHub_BasePreviewData.sections[0].title = STR_LIVE_CHANNELS + ' ' + SmartHub_followerUsername;
         for (i = 0; i < vectorSize; i++) {
 
-            LiveTitle = Main_is_playlist(JSON.stringify(SmartHub_userlive[i].stream_type)) +
+            Title = Main_is_playlist(JSON.stringify(SmartHub_userlive[i].stream_type)) +
                 SmartHub_userlive[i].channel.display_name;
 
-            data += (!i ? '' : ',') + '{"title":"' + LiveTitle + '","subtitle":"' + STR_PLAYING +
-                SmartHub_userlive[i].game + '","image_ratio":"16by9","image_url":"' +
-                (SmartHub_userlive[i].preview.template).replace("{width}x{height}", Main_VideoSize) +
-                '","action_data":"{\\\"videoIdx\\\": \\\"' + SmartHub_userlive[i].channel.name +
-                '\\\",\\\"videoTitleIdx\\\": \\\"' + LiveTitle + '\\\",\\\"_id\\\": \\\"' +
-                SmartHub_userlive[i].channel._id + '\\\"}","is_playable":true}';
+            tile = {
+                "title": Title,
+                "subtitle": STR_PLAYING + SmartHub_userlive[i].game,
+                "image_ratio": "16by9",
+                "image_url": (SmartHub_userlive[i].preview.template).replace("{width}x{height}", Main_VideoSize),
+                "action_data": "{\"videoIdx\": \"" + SmartHub_userlive[i].channel.name +
+                    "\",\"videoTitleIdx\": \"" + Title +
+                    "\",\"_id\": \"" + SmartHub_userlive[i].channel._id + "\"}",
+                "is_playable": true
+            };
 
+            SmartHub_BasePreviewData.sections[0].tiles[i] = tile;
         }
-        data += ']},';
-    }
+    } else delete SmartHub_BasePreviewData.sections[0];
 
+    //Host
     vectorSize = SmartHub_userhost.length;
     if (vectorSize) {
-        data += '{"title":"' + STR_LIVE_HOSTS + ' ' + SmartHub_followerUsername + '","tiles":[';
+        SmartHub_BasePreviewData.sections[1].title = STR_LIVE_HOSTS + ' ' + SmartHub_followerUsername;
         for (i = 0; i < vectorSize; i++) {
-            HostTitle = SmartHub_userhost[i].display_name + STR_USER_HOSTING +
+
+            Title = SmartHub_userhost[i].display_name + STR_USER_HOSTING +
                 SmartHub_userhost[i].target.channel.display_name;
 
-            data += (!i ? '' : ',') + '{"title":"' + HostTitle + '","subtitle":"' + STR_PLAYING +
-                SmartHub_userhost[i].target.meta_game + '","image_ratio":"16by9","image_url":"' +
-                (SmartHub_userhost[i].target.preview_urls.template).replace("{width}x{height}", Main_VideoSize) +
-                '","action_data":"{\\\"videoIdx\\\": \\\"' + SmartHub_userhost[i].target.channel.name +
-                '\\\",\\\"videoTitleIdx\\\": \\\"' + HostTitle +
-                '\\\",\\\"_id\\\": \\\"' + SmartHub_userhost[i].target._id +
-                '\\\",\\\"isHost\\\": \\\"true\\\"}","is_playable":true}';
-        }
-        data += ']},';
-    }
+            tile = {
+                "title": Title,
+                "subtitle": STR_PLAYING + SmartHub_userhost[i].target.meta_game,
+                "image_ratio": "16by9",
+                "image_url": (SmartHub_userhost[i].target.preview_urls.template).replace("{width}x{height}", Main_VideoSize),
+                "action_data": "{\"videoIdx\": \"" + SmartHub_userhost[i].target.channel.name +
+                    "\",\"videoTitleIdx\": \"" + Title +
+                    "\",\"_id\": \"" + SmartHub_userhost[i].target._id +
+                    "\",\"isHost\": \"" + true + "\"}",
+                "is_playable": true
+            };
 
+            SmartHub_BasePreviewData.sections[1].tiles[i] = tile;
+        }
+    } else delete SmartHub_BasePreviewData.sections[1];
+
+    //Games
     vectorSize = SmartHub_usergames.length;
     if (vectorSize) {
-        data += '{"title":"' + STR_LIVE_GAMES + ' ' + SmartHub_followerUsername + '","tiles":[';
+        SmartHub_BasePreviewData.sections[2].title = STR_LIVE_GAMES + ' ' + SmartHub_followerUsername;
         for (i = 0; i < vectorSize; i++) {
-            data += (!i ? '' : ',') + '{"title":"' + SmartHub_usergames[i].game.name +
-                '","image_ratio":"2by3","image_url":"' +
-                (SmartHub_usergames[i].game.box.template).replace("{width}x{height}", Main_GameSize) +
-                '","action_data":"{\\\"gameIdx\\\": \\\"' + SmartHub_usergames[i].game.name +
-                '\\\"}","is_playable":false}';
+
+            tile = {
+                "title": SmartHub_usergames[i].game.name,
+                "image_ratio": "2by3",
+                "image_url": (SmartHub_usergames[i].game.box.template).replace("{width}x{height}", Main_GameSize),
+                "action_data": "{\"gameIdx\": \"" + SmartHub_usergames[i].game.name + "\"}",
+                "is_playable": false
+            };
+
+            SmartHub_BasePreviewData.sections[2].tiles[i] = tile;
         }
-        data += ']},';
-    }
+    } else delete SmartHub_BasePreviewData.sections[2];
 
-    return data + previewDataGeneratorEnd(false);
-}
+    //Clean up "null" section created if used "delete"
+    SmartHub_BasePreviewData.sections = SmartHub_BasePreviewData.sections.filter(function(position) {
+        return position !== null;
+    });
 
-function previewDataGeneratorEnd(IsAddUser) {
-    var UserThumb = '';
-
-    if (IsAddUser) {
-        UserThumb = '{"title":"' + STR_USER_ADD + '","tiles":[' +
-            '{"title":"' + STR_GO_TO + STR_USER_ADD + '","subtitle":"' + STR_ADD_USER_SH + '","image_ratio":"16by9","image_url":"' +
-            IMG_SMART_ADD_USER + '","action_data":"{\\\"screenIdx\\\": ' + Main_addUser + '}","is_playable":false}' +
-            ']},';
-    } else {
-        UserThumb = '{"title":"' + STR_USER + '","tiles":[' +
-            '{"title":"' + STR_GO_TO + STR_USER + '","image_ratio":"16by9","image_url":"' + IMG_SMART_USER +
-            '","action_data":"{\\\"screenIdx\\\": ' + Main_Users + '}","is_playable":false}' +
-            ']},';
-    }
-
-    // new lines separates it blocks to make easier to add new blocks, just copy/paste a block
-
-    return '{"title":"' + STR_LIVE + '","tiles":[' +
-        '{"title":"' + STR_GO_TO + STR_LIVE + '","image_ratio":"16by9","image_url":"' + IMG_SMART_LIVE +
-        '","action_data":"{\\\"screenIdx\\\": ' + Main_Live + '}","is_playable":false}' +
-        ']},' +
-
-        UserThumb +
-
-        '{"title":"' + STR_GAMES + '","tiles":[' +
-        '{"title":"' + STR_GO_TO + STR_GAMES + '","image_ratio":"16by9","image_url":"' + IMG_SMART_GAME +
-        '","action_data":"{\\\"screenIdx\\\": ' + Main_games + '}","is_playable":false}' +
-        ']}' +
-
-        ']}';
+    return JSON.stringify(SmartHub_BasePreviewData);
 }
 
 function SmartHub_EventListener() {
@@ -245,7 +233,7 @@ function SmartHub_EventListener() {
     try {
         requestedAppControl = tizen.application.getCurrentApplication().getRequestedAppControl();
     } catch (e) {}
-    var appControlData, actionData, VideoIdx, GameIdx, ScreenIdx, ExitScreen, ExitToMain = false;
+    var appControlData, actionData, VideoIdx, GameIdx, ScreenIdx, ExitScreen, SwitchScreen = false;
 
     if (requestedAppControl) {
         SmartHub_SmartHubResume = true;
@@ -281,7 +269,7 @@ function SmartHub_EventListener() {
                     actionData = JSON.parse(actionData);
 
                     GameIdx = actionData.gameIdx;
-                    ExitToMain = (GameIdx !== Main_gameSelected);
+                    SwitchScreen = (GameIdx !== Main_gameSelected);
                     ExitScreen = Main_Go;
                     Main_gameSelected = GameIdx;
                     Main_Go = Main_aGame;
@@ -290,24 +278,85 @@ function SmartHub_EventListener() {
                     if (Play_isOn) window.setTimeout(Play_shutdownStream, 10);
                     else if (PlayVod_isOn) window.setTimeout(PlayVod_shutdownStream, 10);
                     else if (PlayClip_isOn) window.setTimeout(PlayClip_shutdownStream, 10);
-                    else if (ExitToMain) Main_SwitchScreen();
+                    else if (SwitchScreen) Main_SwitchScreen();
 
                 } else if (JSON.parse(actionData).screenIdx) {
 
                     actionData = JSON.parse(actionData);
 
                     ScreenIdx = actionData.screenIdx;
-                    ExitToMain = (ScreenIdx !== Main_Go);
+                    SwitchScreen = (ScreenIdx !== Main_Go);
                     ExitScreen = Main_Go;
                     Main_Go = ScreenIdx;
 
-                    Main_ExitCurrent(ExitScreen);
+                    if (SwitchScreen) Main_ExitCurrent(ExitScreen);
+
                     if (Play_isOn) window.setTimeout(Play_shutdownStream, 10);
                     else if (PlayVod_isOn) window.setTimeout(PlayVod_shutdownStream, 10);
                     else if (PlayClip_isOn) window.setTimeout(PlayClip_shutdownStream, 10);
-                    else if (ExitToMain) Main_SwitchScreen();
+                    else if (SwitchScreen) Main_SwitchScreen();
                 }
             }
         }
     }
+}
+
+function SmartHub_SetBasePreviewData() {
+    SmartHub_BasePreviewData = {
+        "sections": [{
+            "title": "Live",
+            "tiles": []
+        }, {
+            "title": "Host",
+            "tiles": []
+        }, {
+            "title": "Games",
+            "tiles": []
+        }, {}, {
+            "title": STR_USER,
+            "tiles": [{
+                "title": STR_GO_TO + STR_USER,
+                "image_ratio": "16by9",
+                "image_url": IMG_SMART_USER,
+                "action_data": "{\"screenIdx\": " + Main_Users + "}",
+                "is_playable": false
+            }]
+        }, {}]
+    };
+    SmartHub_BasePreviewData.sections[3] = SmartHub_BasePreviewDataNoUser.sections[0];
+    SmartHub_BasePreviewData.sections[5] = SmartHub_BasePreviewDataNoUser.sections[2];
+}
+
+function SmartHub_SetNoUserPreviewData() {
+    SmartHub_BasePreviewDataNoUser = {
+        "sections": [{
+            "title": STR_LIVE,
+            "tiles": [{
+                "title": STR_GO_TO + STR_LIVE,
+                "image_ratio": "16by9",
+                "image_url": IMG_SMART_LIVE,
+                "action_data": "{\"screenIdx\": " + Main_Live + "}",
+                "is_playable": false
+            }]
+        }, {
+            "title": STR_USER_ADD,
+            "tiles": [{
+                "title": STR_GO_TO + STR_USER_ADD,
+                "subtitle": STR_ADD_USER_SH,
+                "image_ratio": "16by9",
+                "image_url": IMG_SMART_ADD_USER,
+                "action_data": "{\"screenIdx\": " + Main_addUser + "}",
+                "is_playable": false
+            }]
+        }, {
+            "title": STR_GAMES,
+            "tiles": [{
+                "title": STR_GO_TO + STR_GAMES,
+                "image_ratio": "16by9",
+                "image_url": IMG_SMART_GAME,
+                "action_data": "{\"screenIdx\": " + Main_games + "}",
+                "is_playable": false
+            }]
+        }]
+    };
 }
