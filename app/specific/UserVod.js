f87d2ddd465b19b1aa7c016de2964bc0d67833d2
Felipe Leon <fglfgl27@gmail.com>
2018-08-19 19:47:01 -0300

    uservod correct refresh title
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/UserVod.js
+++ app/specific/UserVod.js
@@ -27,6 +27,7 @@ function UserVod_init() {
     Main_Go = Main_UserVod;
 
     Main_AddClass('top_bar_user', 'icon_center_focus');
+    Main_IconLoad('label_refresh', 'icon-refresh', STR_SWITCH_VOD + STR_GUIDE);
     Main_IconLoad('label_controls', 'icon-calendar', STR_SWITCH_TYPE + ' (C)');
     Main_innerHTML('top_bar_user', STR_USER + Main_UnderCenter(Main_UserName + (UserVod_highlight ? STR_PAST_HIGHL : STR_PAST_BROA)));
     document.body.addEventListener("keydown", UserVod_handleKeyDown, false);
@@ -41,6 +42,7 @@ function UserVod_init() {
 
 function UserVod_exit() {
     Main_RemoveClass('top_bar_user', 'icon_center_focus');
+    Main_IconLoad('label_refresh', 'icon-refresh', STR_REFRESH + STR_GUIDE);
     Main_IconLoad('label_controls', 'icon-question-circle', STR_CONTROL_KEY);
     document.body.removeEventListener("keydown", UserVod_handleKeyDown);
     Main_textContent('top_bar_user', STR_USER);
