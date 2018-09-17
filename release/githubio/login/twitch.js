0de8930acdc22cf3df08ad65d7e2f243e35bb1e8
Felipe Leon <fglfgl27@gmail.com>
2018-09-17 12:13:08 -0300

    Update release/githubio/login/twitch.js
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- release/githubio/login/twitch.js
+++ release/githubio/login/twitch.js
@@ -1,23 +1,25 @@
-/* jshint undef: true, unused: true, node: true, browser: true */
-// Keep this file named as (zero)*** so it loads first in release_maker
+/* jshint eqeqeq: true, undef: true, unused: true, node: true, browser: true */
 
-var baseUrl = 'https://id.twitch.tv/oauth2';
-
-//vars to get the code
-var baseUrlCode = baseUrl + '/authorize?';
+var baseUrlCode = 'https://id.twitch.tv/oauth2/authorize?';
 var type_code = 'code';
 var client_id = 'ypvnuqrh98wqz1sr0ov3fgfu4jh1yx';
 var redirect_uri = 'https://fgl27.github.io/smarttv-twitch/release/githubio/login/twitch.html';
 var scope = 'user_follows_edit+user_subscriptions';
 var force_verify = 'true';
-var client_secret = "zhd1wr8lxyz9snzo48rfb70r7vtod6";
 var code = '';
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Process Started");
+Start();
+
+function Start() {
+    if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", ProcessStart);
+    else ProcessStart(); // `DOMContentLoaded` already fired
+}
+
+function ProcessStart() {
+    console.log("ProcessStart");
     if (window.location.href.indexOf('code') !== -1) processCode(window.location.href);
     else document.getElementById("Authorize").addEventListener("click", autorizationClick);
-});
+}
 
 function autorizationClick() {
     console.log("autorizationClick");
@@ -32,10 +34,11 @@ function processCode(pageUrl) {
     if (code) {
         code = code[1];
         document.getElementById('Authorize').classList.add('hidden');
-        document.getElementById("PageText").innerHTML = 'Enter the bellow key in the above screen of the app (There is only lower case letters)<br><div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">Key:<br><br>' + code + '<br></div>';
-        console.log(code);
+        document.getElementById("PageText").innerHTML = 'Enter the bellow key in the above screen of the app (There is only lower case letters and numbers)<br><div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">Key:<br><br>' + code + '<br></div>';
+        console.log('if code ' + code);
     } else {
         document.getElementById("PageText").innerHTML = '<div style="font-size: 150%; font-weight: bold; color: #FFFFFF;">An error occurred, please try again, if the error persists contact the developer in the bellow github link</div>';
         document.getElementById("Authorize").addEventListener("click", autorizationClick);
+        console.log('else code ' + code);
     }
 }
