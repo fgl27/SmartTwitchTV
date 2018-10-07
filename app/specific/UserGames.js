1d29acd78e77e248296faef8a429bd9e5f266de9
Felipe Leon <fglfgl27@gmail.com>
2018-10-07 13:52:44 -0300

    Workaround to load all live channels
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/UserGames.js
+++ app/specific/UserGames.js
@@ -82,14 +82,15 @@ function UserGames_loadDataRequest() {
         var xmlHttp = new XMLHttpRequest();
 
         var offset = UserGames_itemsCount + UserGames_itemsCountOffset;
-        if (offset && offset > (UserGames_MaxOffset - 1)) {
+        if (!UserGames_live && offset && offset > (UserGames_MaxOffset - 1)) {
             offset = UserGames_MaxOffset - Main_ItemsLimitGame;
             UserGames_dataEnded = true;
         }
 
         xmlHttp.open("GET", 'https://api.twitch.tv/api/users/' + encodeURIComponent(AddUser_UsernameArray[Users_Position].name) +
-            '/follows/games' + (UserGames_live ? '/live' : '') + '?limit=' + (Main_ItemsLimitGame + Main_ItemsLimitGameOffset) +
+            '/follows/games' + (UserGames_live ? '/live?limit=' + ((Main_ItemsLimitGame * 2) + Main_ItemsLimitGameOffset) : +'?limit=' + (Main_ItemsLimitGame + Main_ItemsLimitGameOffset)) +
             '&offset=' + offset + '&' + Math.round(Math.random() * 1e7), true);
+
         xmlHttp.timeout = UserGames_loadingDataTimeout;
         xmlHttp.setRequestHeader(Main_AcceptHeader, Main_TwithcV5Json);
         xmlHttp.setRequestHeader(Main_clientIdHeader, Main_clientId);
@@ -135,18 +136,23 @@ function UserGames_loadDataSuccess(responseText) {
     var response_items = response.follows.length;
     UserGames_MaxOffset = parseInt(response._total);
 
+    //TODO revise this
+    //Workaround to load all live channel, twitch send this on alphabetical order theoretically do not have a limit
+    var limit = Main_ItemsLimitGame;
+    if (UserGames_live) limit = Main_ItemsLimitGame * 2;
+
     //TODO improve this
     //The correct fix here is to save the result of Games_loadDataRequest to a object
     //Only add to that object new games to prevent duplicated and from that load the page
     //this way we request more then what we need and keep request at will to keep what we have bigger then what we will need next
     //But that will take some time to write and I don't have any
-    if (response_items > Main_ItemsLimitGame) {
+    if (response_items > limit) {
         Main_ItemsLimitGameOffset--;
         UserGames_loadDataPrepare();
         UserGames_loadDataRequest();
         return;
-    } else if (response_items < Main_ItemsLimitGame) {
-        if (((Main_ItemsLimitGame - response_items) < 11) && (Main_ItemsLimitGameOffset + Main_ItemsLimitGame) < 100 && (UserGames_MaxOffset - 1) > (response_items + UserGames_itemsCount)) {
+    } else if (response_items < limit) {
+        if (((limit - response_items) < 11) && (Main_ItemsLimitGameOffset + limit) < 100 && (UserGames_MaxOffset - 1) > (response_items + UserGames_itemsCount)) {
             Main_ItemsLimitGameOffset++;
             UserGames_loadDataPrepare();
             UserGames_loadDataRequest();
