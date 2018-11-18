57d294372b99988a7c05847cc0ad70413c627bc8
Felipe Leon <fglfgl27@gmail.com>
2018-11-17 23:34:44 -0200

    Up master.js
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -105,7 +105,7 @@ var Main_WasOpen = false;
 var Main_version = 401;
 var Main_stringVersion = '4.0.1';
 var Main_currentVersion = '';
-var Main_minversion = '111618';
+var Main_minversion = '111718';
 var Main_versonTag = '';
 var Main_TizenVersion;
 var Main_ClockOffset = 0;
