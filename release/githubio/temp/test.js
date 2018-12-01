bf49c8443afd0b35de298c100565a47dccf52a3a
Felipe Leon <fglfgl27@gmail.com>
2018-11-30 23:14:49 -0200

    Up test page
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- release/githubio/temp/test.js
+++ release/githubio/temp/test.js
@@ -5,83 +5,67 @@ var Main_clientIdHeader = 'Client-ID';
 var Main_AcceptHeader = 'Accept';
 var Main_TwithcV5Json = 'application/vnd.twitchtv.v5+json';
 
-// problematic games with ,,,,,,,,
-var game1 = "506237";//"Pokemon: Let's Go, Pikachu!/Eevee!";
-var game2 = "Warhammer 40,000: Mechanicus";
+// problematic games contains ","
 
-//OK games
-var game3 = "Fortnite";
-var game4 = "Dota 2";
+var game = ["Pokemon: Let's Go, Pikachu!/Eevee!", "Warhammer 40,000: Mechanicus", "Counter-Strike: Global Offensive", "Dota 2"];
 
-//console.log(Settings_GenerateClock());
+Start();
 
-function getclips() {
+function Start() {
+    if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", getAclip);
+    else getAclip(); // `DOMContentLoaded` already fired
+}
+
+function getAclip() {
     var xmlHttp = new XMLHttpRequest();
 
-//    var url = 'https://api.twitch.tv/kraken/clips/top?game=';
-//    url += game1 + '&limit=100&period=all';
+    var url = 'https://api.twitch.tv/kraken/clips/top?game=';
 
-    var started_at = new Date().getTime() - (1000 * 60 * 60 * 24 * 5);
-    var ended_at = new Date().getTime() - (1000 * 60 * 60 * 24 * 2);
+    var code = window.location.href.match(/game=(\w+)/);
+    var mgame = '';
 
-    var url = 'https://api.twitch.tv/helix/clips?game_id=';
-    url += game1 + '&first=100&started_at=' + encodeURIComponent((new Date(started_at)).toISOString()) +
-    '&ended_at=' + encodeURIComponent((new Date()).toISOString());
+    if (code && code[1] < 4) mgame += game[code[1]];
+    else mgame += game[0];
 
-    console.log(url);
+    url += encodeURIComponent(mgame);
 
     xmlHttp.open("GET", url, true);
 
     xmlHttp.timeout = 10000;
     xmlHttp.setRequestHeader(Main_clientIdHeader, Main_clientId);
     xmlHttp.setRequestHeader(Main_AcceptHeader, Main_TwithcV5Json);
-    xmlHttp.setRequestHeader("Accept-Language", 'hu');
-
     xmlHttp.ontimeout = function() {};
 
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState === 4) {
-            console.log(xmlHttp.status);
             console.log(xmlHttp.responseText);
-            //var data = JSON.parse(xmlHttp.responseText).clips;
-            //console.log('quantity ' + data.length);
+
+            var data = JSON.parse(xmlHttp.responseText).clips;
+
+            var hascomma = mgame.indexOf(',') !== -1 ? "This Game has a comma" : "This Game doesn't has a comma";
+
+            hascomma += " result will return " + data.length + " clips";
+
+            var ele = document.getElementById('result');
+            ele.innerHTML = addtitle() + redDiv(hascomma) + '<br>xmlHttp.status: ' +
+                redDiv(xmlHttp.status) + 'api url call: ' + redDiv(url) +
+                'clips quantity: ' + redDiv(data.length) + 'game: ' + redDiv(mgame) +
+                '<br><br> In the log you can see and copy the json result.';
         }
     };
 
     xmlHttp.send(null);
 }
 
-//console.log((new Date()).toISOString());
+function redDiv(text) {
+    return '<div style="display: inline-block; color: #FF0000; font-size: 110%; font-weight: bold;">' + text + '</div><br>';
+}
 
-getclips();
+function addtitle() {
+    var title = 'Possible call options are the bellow links:<br><br>';
 
-var clip_slug = 'AmericanPricklyBaguetteSeemsGood';
+    for (var i = 0; i < game.length; i++)
+        title += '<a href="https://fgl27.github.io/smarttv-twitch/release/githubio/temp/test.html?game=' + i + '">For: ' + game[i] + '</a><br>';
 
-//getAclip();
-
-function getAclip() {
-    var xmlHttp = new XMLHttpRequest();
-
-    var url = 'https://api.twitch.tv/kraken/clips/';
-    url += clip_slug;
-
-    console.log(url);
-
-    xmlHttp.open("GET", url, true);
-
-    xmlHttp.timeout = 10000;
-    xmlHttp.setRequestHeader(Main_clientIdHeader, Main_clientId);
-    xmlHttp.setRequestHeader(Main_AcceptHeader, Main_TwithcV5Json);
-    xmlHttp.ontimeout = function() {};
-
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4) {
-            console.log(xmlHttp.status);
-            console.log(xmlHttp.responseText);
-            //var data = JSON.parse(xmlHttp.responseText).clips;
-            //console.log('quantity ' + data.length);
-        }
-    };
-
-    xmlHttp.send(null);
+    return title + '<br>';
 }
