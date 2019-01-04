8217a4374f1b2f36ed2aea430e1b358ba222c52b
Felipe Leon <fglfgl27@gmail.com>
2019-01-04 16:28:08 -0200

    Revert "Update logs"
    This reverts commit 12e9746ee17e88a4f9b1970a41ae45bff8193918.

--- app/specific/PlayClip.js
+++ app/specific/PlayClip.js
@@ -228,7 +228,7 @@ function PlayClip_qualityChanged() {
 
 function PlayClip_onPlayer() {
     Play_showBufferDialog();
-    if (!Main_isReleased) console.log('PlayClip_onPlayer:', '\n' + '\n"' + PlayClip_playingUrl + '"\n');
+    if (!Main_isReleased) console.log('PlayClip_onPlayer:', '\n' + '\n' + PlayClip_playingUrl + '\n');
     try {
         Play_avplay.stop();
         Play_avplay.open(PlayClip_playingUrl);
