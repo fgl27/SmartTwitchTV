8df334b1b9541bb873f8ba0e0ce2ce9c80434268
Felipe Leon <fglfgl27@gmail.com>
2019-01-24 12:20:04 -0200

    Play try to prevent empty streamer name
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -244,7 +244,8 @@ function Play_Start() {
     Play_LoadLogoSucess = false;
     //reset channel logo to prevent another channel logo
     Play_LoadLogo(document.getElementById('stream_info_icon'), IMG_404_LOGO);
-    Main_textContent("stream_info_name", (Main_values.Play_isHost ? Main_values.Play_DisplaynameHost : Main_values.Play_selectedChannelDisplayname));
+    if (Main_values.Play_isHost) Main_textContent("stream_info_name", Main_values.Play_DisplaynameHost);
+    else Main_textContent("stream_info_name", Main_values.Play_selectedChannelDisplayname);
 
     Main_values.Play_WasPlaying = 1;
     Main_SaveValues();
@@ -345,6 +346,9 @@ function Play_updateStreamInfoStart() {
             if (xmlHttp.status === 200) {
                 var response = JSON.parse(xmlHttp.responseText);
                 if (response.stream !== null) {
+                    if (Main_values.Play_isHost) Main_textContent("stream_info_name", Main_values.Play_DisplaynameHost);
+                    else Main_textContent("stream_info_name", Main_values.Play_selectedChannelDisplayname);
+
                     Main_values.Play_selectedChannel_id = response.stream.channel._id;
                     Main_innerHTML("stream_info_title", twemoji.parse(response.stream.channel.status));
                     Main_values.Play_gameSelected = response.stream.game;
