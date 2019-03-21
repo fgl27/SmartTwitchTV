936cc6ca86a744c3be0747e0f9ef651fcf15971b
Felipe Leon <fglfgl27@gmail.com>
2019-03-21 11:21:02 -0300

    Add a base apk
    it can load the webview and play a live for now
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- apk/README.md
+++ apk/README.md
@@ -1,1 +1,14 @@
-A apk is be designed it's code will be shared here when it's ready for testing
+SmartTwitchTV AndroidTV APK
+==============
+
+This is a WIP apk
+
+## Download/Clone & Build
+
+``` bash
+$ git clone git://github.com/fgl27/SmartTwitchTV.git
+$ cd SmartTwitchTV/apk
+$ ./gradlew build
+```
+
+**Android Studio build is also supported**, but I personalty prefer build using a Linux terminal, if you can't do that and the app doesn't build on Studio [open a issue](https://github.com/fgl27/SmartTwitchTV/issues/new/choose).
---
+++ apk/app/src/main/java/com/fgl27/twitchtv/PlaybackVideoFragment.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.fgl27.twitchtv;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v17.leanback.app.VideoSupportFragment;
+import android.support.v17.leanback.app.VideoSupportFragmentGlueHost;
+import android.support.v17.leanback.media.MediaPlayerAdapter;
+import android.support.v17.leanback.media.PlaybackTransportControlGlue;
+import android.support.v17.leanback.widget.PlaybackControlsRow;
+
+/**
+ * Handles video playback with media controls.
+ */
+public class PlaybackVideoFragment extends VideoSupportFragment {
+
+    private PlaybackTransportControlGlue<MediaPlayerAdapter> mTransportControlGlue;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       final String url = getActivity().getIntent().getStringExtra("url");
+
+        VideoSupportFragmentGlueHost glueHost =
+                new VideoSupportFragmentGlueHost(PlaybackVideoFragment.this);
+
+        MediaPlayerAdapter playerAdapter = new MediaPlayerAdapter(getActivity());
+        playerAdapter.setRepeatAction(PlaybackControlsRow.RepeatAction.INDEX_NONE);
+
+        mTransportControlGlue = new PlaybackTransportControlGlue<>(getActivity(), playerAdapter);
+        mTransportControlGlue.setHost(glueHost);
+        mTransportControlGlue.setTitle("title");
+        mTransportControlGlue.setSubtitle("description");
+        mTransportControlGlue.playWhenPrepared();
+        playerAdapter.setDataSource(Uri.parse(url));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mTransportControlGlue != null) {
+            mTransportControlGlue.pause();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mTransportControlGlue != null) {
+            mTransportControlGlue.play();
+        }
+    }
+}
