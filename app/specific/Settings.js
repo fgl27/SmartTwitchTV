5407e44c8db2b4846f86d9bb56195414ecb74997
Felipe Leon <fglfgl27@gmail.com>
2019-04-24 11:45:05 -0300

    Settings lower buffer default size
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Settings.js
+++ app/specific/Settings.js
@@ -23,11 +23,11 @@ var Settings_value = {
     },
     "buffer_live": { //buffer_live
         "values": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        "defaultValue": 6
+        "defaultValue": 4
     },
     "buffer_vod": { //buffer_vod
         "values": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        "defaultValue": 6
+        "defaultValue": 4
     },
     "buffer_clip": { //buffer_clip
         "values": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
