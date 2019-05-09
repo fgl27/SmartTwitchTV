8ec859febedc88d1e814ce151983a9c1f3fd451c
Felipe Leon <fglfgl27@gmail.com>
2019-05-09 17:35:55 -0300

    PlayerActivity deprecated and static leak dewarning
    new exoplayer version new warning and leaks
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
+++ apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
@@ -38,8 +38,8 @@ import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.mediacodec.MediaCodecRenderer.DecoderInitializationException;
 import com.google.android.exoplayer2.mediacodec.MediaCodecUtil.DecoderQueryException;
 import com.google.android.exoplayer2.source.BehindLiveWindowException;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
@@ -77,7 +77,7 @@ public class PlayerActivity extends Activity {
     private PlayerView simpleExoPlayerView;
     public static SimpleExoPlayer player;
     private DataSource.Factory dataSourceFactory;
-    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private DefaultBandwidthMeter BANDWIDTH_METER;
 
     private DefaultTrackSelector trackSelector;
     private boolean shouldAutoPlay;
@@ -105,6 +105,8 @@ public class PlayerActivity extends Activity {
             setContentView(R.layout.activity_player);
             url = "file:///android_asset/temp.mp4";
 
+            //BANDWIDTH_METER =  new DefaultBandwidthMeter();
+            BANDWIDTH_METER =  new DefaultBandwidthMeter.Builder(this).build();
             dataSourceFactory =
                     new DefaultDataSourceFactory(
                             this, Util.getUserAgent(this, this.getString(R.string.app_name)));
@@ -342,7 +344,7 @@ public class PlayerActivity extends Activity {
             case C.TYPE_HLS:
                 return new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
             case C.TYPE_OTHER:
-                return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+                return new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
             default:
                 throw new IllegalStateException("Unsupported type: " + type);
         }
