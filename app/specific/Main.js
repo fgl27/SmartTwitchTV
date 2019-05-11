8e9ea4adcd3750e4b876c1ef72d73f9b8df25a94
Felipe Leon <fglfgl27@gmail.com>
2019-05-10 21:01:22 -0300

    Up master files
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -121,7 +121,7 @@ var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '1.0';
 var Main_stringVersion_Min = '.19';
-var Main_minversion = '050919';
+var Main_minversion = '051019';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_AndroidVersion = '';
 var Main_ClockOffset = 0;
