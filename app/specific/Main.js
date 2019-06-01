a00ab3435647347380601eea80cffe88fabe03b1
Felipe Leon <fglfgl27@gmail.com>
2019-06-01 16:03:03 -0300

    fix document.hidden check in Main_updateUserFeed
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -1028,16 +1028,14 @@ function Main_updateclock() {
 }
 
 function Main_updateUserFeed() {
-    if (!document.hidden) {
         if (AddUser_UserIsSet()) {
             window.setTimeout(function() {
-                if (!UserLiveFeed_isFeedShow() && !UserLiveFeed_loadingData) {
+                if (!document.hidden && !UserLiveFeed_isFeedShow() && !UserLiveFeed_loadingData) {
                     Play_FeedOldUserName = AddUser_UsernameArray[Main_values.Users_Position].name;
                     UserLiveFeed_StartLoad();
                 }
             }, 5000);
         }
-    }
 }
 
 function Main_ExitDialog(event) {
