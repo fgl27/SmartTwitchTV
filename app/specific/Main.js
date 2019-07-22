45f21044b640a712542a1af6eb73bca2b9cf6f1b
Felipe Leon <fglfgl27@gmail.com>
2019-07-22 16:28:54 -0300

    Up publishVersionCode 34
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -112,7 +112,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '1.0';
-var Main_stringVersion_Min = '.33';
+var Main_stringVersion_Min = '.34';
 var Main_minversion = '072219';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_AndroidVersion = '';
