599179641b103f75c135d5a7c452f741550331b6
Felipe Leon <fglfgl27@gmail.com>
2019-07-30 14:16:48 -0300

    Add lazyload
    for testing if there is load performance improve
    from:
    https://github.com/verlok/lazyload
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -121,7 +121,7 @@ var Main_randomimg = '?' + Math.random();
 var proxyurl = "https://cors-anywhere.herokuapp.com/";
 var Main_updateUserFeedId;
 var Main_vp9supported = false;
-
+var lazyLoadInstance;
 //Variable initialization end
 
 // this function will be called only once the first time the app startup
@@ -201,6 +201,10 @@ function Main_loadTranslations(language) {
 }
 
 function Main_initWindows() {
+    lazyLoadInstance = new LazyLoad({
+        elements_selector: ".lazy"
+    });
+
     Screens_InitScreens();
     Main_SetStringsMain(true);
 
