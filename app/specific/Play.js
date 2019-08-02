0169116effc746bd1049b88ea5dfc6671b29d23a
Felipe Leon <fglfgl27@gmail.com>
2019-08-02 17:13:11 -0300

    fix usher.twitch link
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -438,7 +438,7 @@ function Play_loadDataRequest() {
             (AddUser_UserIsSet() && AddUser_UsernameArray[Main_values.Users_Position].access_token ? '&oauth_token=' +
                 AddUser_UsernameArray[Main_values.Users_Position].access_token : '');
     } else {
-        theUrl = 'http://usher.twitch.net/api/channel/hls/' + Main_values.Play_selectedChannel +
+        theUrl = 'http://usher.twitch.tv/api/channel/hls/' + Main_values.Play_selectedChannel +
             '.m3u8?&token=' + encodeURIComponent(Play_tokenResponse.token) + '&sig=' + Play_tokenResponse.sig +
             '&player=twitchweb&type=any&playlist_include_framerate=true&allow_source=true' +
             (Main_vp9supported ? '&preferred_codecs=vp09' : '') + '&p=' + Main_RandomInt();
