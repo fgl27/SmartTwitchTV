2f657e24a606f158fc4c4c4a3618f4b055e05d17
Felipe Leon <fglfgl27@gmail.com>
2019-08-03 14:26:21 -0300

    Up publishVersionCode 42
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -111,7 +111,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '1.0';
-var Main_stringVersion_Min = '.41';
+var Main_stringVersion_Min = '.42';
 var Main_minversion = '080219';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
