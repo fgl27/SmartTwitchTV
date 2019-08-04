15d2755d25269b84487bb33d378f4fdfdcaf75e4
Felipe Leon <fglfgl27@gmail.com>
2019-08-04 19:53:48 -0300

    clean up logs
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/ScreensObj.js
+++ app/specific/ScreensObj.js
@@ -83,7 +83,6 @@ var Base_obj = {
         document.body.removeEventListener("keydown", Screens_handleKeyDown);
     },
     concatenate: function(responseText) {
-    console.log(responseText);
         if (this.data) {
             responseText = JSON.parse(responseText);
 
