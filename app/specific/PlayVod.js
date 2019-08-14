bc77dc6b697824c3819a90c3fa0166c4d3ab226e
Felipe Leon <fglfgl27@gmail.com>
2019-08-14 19:29:27 -0300

    prevent crash from player _SetHtmlQuality
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/PlayVod.js
+++ app/specific/PlayVod.js
@@ -618,7 +618,7 @@ function PlayVod_qualityDisplay() {
 }
 
 function PlayVod_SetHtmlQuality(element) {
-    if (!PlayVod_qualities[PlayVod_qualityIndex].hasOwnProperty('id')) return;
+    if (!PlayVod_qualities[PlayVod_qualityIndex] || !PlayVod_qualities[PlayVod_qualityIndex].hasOwnProperty('id')) return;
 
     PlayVod_quality = PlayVod_qualities[PlayVod_qualityIndex].id;
 
