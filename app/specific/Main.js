577db6242a37a61cdf6a7fc22ec2a74cb19527a2
Felipe Leon <fglfgl27@gmail.com>
2019-09-08 12:29:46 -0300

    Up mater files
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -111,7 +111,7 @@ var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
 var Main_stringVersion_Min = '.65';
-var Main_minversion = '090719';
+var Main_minversion = '090819';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_ClockOffset = 0;
