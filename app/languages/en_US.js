5481f3aaaddb839bdd3afcc870a21910b01c5dfc
Felipe Leon <fglfgl27@gmail.com>
2019-09-11 14:21:54 -0300

    Improve strings
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/languages/en_US.js
+++ app/languages/en_US.js
@@ -310,7 +310,7 @@ function en_USLang() {
     STR_FORBIDDEN = "If start getting buffers issue disable " + STR_LOW_LATENCY +
         "<br>Use " + STR_SETTINGS_BUFFER_LIVE + " equal or bellow to 1 for this to have effect";
     STR_LIVE_FEED_SORT = "Live feed sort";
-    STR_LIVE_FEED_SORT_SUMMARY = "Sorts both side panel live feed as player live feed";
+    STR_LIVE_FEED_SORT_SUMMARY = "Sorts side panel live feed and player live feed";
     STR_A_Z = "Alphabetical A - Z";
     STR_Z_A = "Alphabetical Z - A";
 }
