9de12759265b2dcfdc3bba278cd56dca0d698e78
Felipe Leon <fglfgl27@gmail.com>
2019-09-16 18:35:35 -0300

    add missing alt to img
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -192,7 +192,7 @@ function Play_PreStart() {
     Play_SetAudioIcon();
 
     Main_innerHTML('user_feed_notify_img_holder',
-        '<img id="user_feed_notify_img" class="notify_img" src="' + IMG_404_LOGO +
+        '<img id="user_feed_notify_img" alt="" class="notify_img" src="' + IMG_404_LOGO +
         '" onerror="this.onerror=null;this.src=\'' + IMG_404_LOGO + '\'" >');
 }
 
