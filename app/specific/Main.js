f8ff9e56179d00ccca59d486034587d21ee38f6f
Felipe Leon <fglfgl27@gmail.com>
2019-10-19 23:44:52 -0300

    improve Main_Checktylesheet and remove log
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -1,6 +1,6 @@
 //Variable initialization
 var Main_isReleased = false;
-var Main_isDebug = false;
+var Main_isDebug = true;
 
 var Main_cursorYAddFocus = -1;
 
@@ -137,6 +137,7 @@ function Main_Start() {
 
 function Main_loadTranslations(language) {
     Main_Checktylesheet();
+    Main_isDebug = false;
 
     Main_ready(function() {
         try {
