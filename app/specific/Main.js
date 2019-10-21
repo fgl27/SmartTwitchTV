e54642c588dce70ebd8bd73f7ce9c6a5448e252d
Felipe Leon <fglfgl27@gmail.com>
2019-10-21 11:47:37 -0300

    Up publishVersionCode 81
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -109,7 +109,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
-var Main_stringVersion_Min = '.80';
+var Main_stringVersion_Min = '.81';
 var Main_minversion = '101719';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
