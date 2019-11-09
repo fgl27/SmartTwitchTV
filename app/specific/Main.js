529d5c76fec701d9b48bd2a8d78544c1fd2a8234
Felipe Leon <fglfgl27@gmail.com>
2019-11-08 21:44:10 -0300

    Up publishVersionCode 90
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -112,7 +112,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
-var Main_stringVersion_Min = '.88';
+var Main_stringVersion_Min = '.90';
 var Main_minversion = '110619';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
