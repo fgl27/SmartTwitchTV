dca15d6f296d38af2972f10e4685173ea85f140b
Felipe Leon <fglfgl27@gmail.com>
2019-12-04 13:49:05 -0300

    UP publishVersionCode 95
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -113,8 +113,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
-var Main_stringVersion_Min = '.94';
-var Main_minversion = '111819';
+var Main_stringVersion_Min = '.95';
+var Main_minversion = '120419';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_ClockOffset = 0;
