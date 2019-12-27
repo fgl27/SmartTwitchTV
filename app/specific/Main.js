b26324b29549abd9a823a26f35637145e698ecab
Felipe Leon <fglfgl27@gmail.com>
2019-12-27 14:37:57 -0300

    Up publishVersionCode 104
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -118,8 +118,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
-var Main_stringVersion_Min = '.103';
-var Main_minversion = '122619';
+var Main_stringVersion_Min = '.104';
+var Main_minversion = '122719';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_ClockOffset = 0;
