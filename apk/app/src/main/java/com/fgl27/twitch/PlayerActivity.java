c46786f61a6ccc820870de9d8547d03d35581fdb
Felipe Leon <fglfgl27@gmail.com>
2019-12-28 19:02:10 -0300

    Add hideSystemUI function
    to fully enable immersive layout on old android
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
+++ apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
@@ -536,6 +536,36 @@ public class PlayerActivity extends Activity {
         ClearPlayer(1);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && !mdeviceIsTV()) {
+            hideSystemUI();
+        }
+    }
+
+    public boolean mdeviceIsTV() {
+        UiModeManager uiModeManager = (UiModeManager) getSystemService(UI_MODE_SERVICE);
+        return (uiModeManager != null ? uiModeManager.getCurrentModeType() : 0) == Configuration.UI_MODE_TYPE_TELEVISION;
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
     //Close the app
     private void closeThis() {
         finishAndRemoveTask();
