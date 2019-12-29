d4fb2990fda802f6353d4a7afa16b2a7dad0e7f1
Felipe Leon <fglfgl27@gmail.com>
2019-12-29 12:29:50 -0300

    hideSystemUI use SYSTEM_UI_FLAG_IMMERSIVE_STICKY
    prevents from nav keys be persistent ovr the app
    wth SYSTEM_UI_FLAG_IMMERSIVE_STICKY they show they go away after a second
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
+++ apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
@@ -576,7 +576,7 @@ public class PlayerActivity extends Activity {
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
