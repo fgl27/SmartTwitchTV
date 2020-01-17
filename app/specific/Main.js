1193831ac5f4cde605e80ac26def493cc7b59d0d
fgl27 <fglfgl27@gmail.com>
2020-01-17 13:59:30 -0300

    Up publishVersionCode 108
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -128,8 +128,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '2.0';
-var Main_stringVersion_Min = '.107';
-var Main_minversion = '011120';
+var Main_stringVersion_Min = '.108';
+var Main_minversion = '011720';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_AndroidSDK = 1000;
