d3c32dfde0117e8c12c86187c906b3a4c52ccde7
fgl27 <fglfgl27@gmail.com>
2020-02-06 14:42:08 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -11359,7 +11359,7 @@
     function Screens_createCellClip(id, idArray, valuesArray, Extra_when, Extra_until) {
         var playing = (valuesArray[3] !== "" ? STR_PLAYING + valuesArray[3] : "");
         //Clips images fails with CORS
-        Screens_PreloadImgsArray.push(valuesArray[15]);
+        //Screens_PreloadImgsArray.push(valuesArray[15]);
 
         return Screens_createCell(
             idArray[8] + id,
