416302c754f6d7e641fdd9f6a45a5f679340fa3f
fgl27 <fglfgl27@gmail.com>
2020-02-07 02:03:24 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -4455,8 +4455,7 @@
             }
         }
 
-        Main_values.Play_isHost = ((Main_values.Main_Go === Main_UserHost) && !Play_UserLiveFeedPressed) ||
-            ((UserLiveFeed_FeedPosX === UserLiveFeedobj_UserHostPos) && Play_UserLiveFeedPressed);
+        Main_values.Play_isHost = (Play_data.data[1].indexOf(STR_USER_HOSTING) !== -1);
 
         if (Main_values.Play_isHost) {
             Play_data.DisplaynameHost = document.getElementById(idsArray[3] + id).textContent;
@@ -12590,7 +12589,7 @@
         Main_textContent('dialog_thumb_opt_val_2', '...');
 
         if (inUseObj.screenType < 2) {
-            Main_values.Play_isHost = (Main_values.Main_Go === Main_UserHost) && !Play_UserLiveFeedPressed;
+            Main_values.Play_isHost = (Screens_values_Play_data[1].indexOf(STR_USER_HOSTING) !== -1);
 
             if (Main_values.Play_isHost) {
                 Main_textContent('dialog_thumb_opt_val_0', Screens_values_Play_data[1].split(STR_USER_HOSTING)[1]);
@@ -12871,7 +12870,7 @@
         if (inUseObj.screenType < 2) {
             Main_values.Main_selectedChannel_id = Screens_values_Play_data[14];
 
-            Main_values.Play_isHost = (Main_values.Main_Go === Main_UserHost) && !Play_UserLiveFeedPressed;
+            Main_values.Play_isHost = (Screens_values_Play_data[1].indexOf(STR_USER_HOSTING) !== -1);
 
             if (Main_values.Play_isHost) {
                 Main_values.Main_selectedChannelDisplayname = Screens_values_Play_data[1].split(STR_USER_HOSTING)[1];
