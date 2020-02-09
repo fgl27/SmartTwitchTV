e503237c05c8132e0267ab26cd8dd3be49c3e245
fgl27 <fglfgl27@gmail.com>
2020-02-09 15:18:30 -0300

    UP master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -125,7 +125,7 @@ var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
 var Main_stringVersion_Min = '.110';
-var Main_minversion = '020820';
+var Main_minversion = '020920';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_AndroidSDK = 1000;
