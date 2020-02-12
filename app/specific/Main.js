ae6645a938ff90fb2925716ed8fb7d5a98a18c1c
fgl27 <fglfgl27@gmail.com>
2020-02-12 14:16:03 -0300

    Improve user_feed_hide and Main_updateUserFeed
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -1565,12 +1565,9 @@ function Main_RandomInt() {
 }
 
 function Main_updateUserFeed() {
-    if (AddUser_UserIsSet()) {
-        window.setTimeout(function() {
-            if (!document.hidden && !UserLiveFeed_isFeedShow() && !Sidepannel_isShowing() && !UserLiveFeed_loadingData) {
-                UserLiveFeed_RefreshLive();
-            }
-        }, 15000);
+    if (!document.hidden && AddUser_UserIsSet() && !UserLiveFeed_isFeedShow() &&
+        !Sidepannel_isShowing() && !UserLiveFeed_loadingData) {
+        UserLiveFeed_RefreshLive();
     }
 }
 
