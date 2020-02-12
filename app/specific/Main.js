8ad147e98f4757864a36ba264e44cb8552d4f375
fgl27 <fglfgl27@gmail.com>
2020-02-12 14:55:51 -0300

    Improve user_feed performance
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -1353,10 +1353,12 @@ function Main_isScene1DocShown() {
 
 function Main_showScene2Doc() {
     Main_ShowElementWithEle(Main_Scene2Doc);
+    Main_ShowElementWithEle(UserLiveFeed_FeedHolderDocId);
 }
 
 function Main_hideScene2Doc() {
     Main_HideElementWithEle(Main_Scene2Doc);
+    Main_HideElementWithEle(UserLiveFeed_FeedHolderDocId);
 }
 
 function Main_isScene2DocShown() {
