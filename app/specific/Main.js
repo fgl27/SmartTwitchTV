ec9896725d1ef41e180c2bc8fac8beb4e36eab59
fgl27 <fglfgl27@gmail.com>
2020-02-21 14:05:59 -0300

    Up publishVersionCode 115
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -124,8 +124,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.114';
-var Main_minversion = '021820';
+var Main_stringVersion_Min = '.115';
+var Main_minversion = '022120';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_AndroidSDK = 1000;
