479f20eb51d3c09ca0129b4256030e018109529e
fgl27 <fglfgl27@gmail.com>
2020-02-27 16:19:19 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -6246,9 +6246,6 @@
 
     function PlayExtra_Resumenew() {
         if (Main_IsNotBrowser) {
-            Android.mSwitchPlayerAudio(Play_controlsAudioPos);
-            PlayExtra_data.watching_time = new Date().getTime();
-            Play_SetAudioIcon();
 
             var StreamData = Play_getStreamData(PlayExtra_data.data[6], true);
 
@@ -6282,6 +6279,9 @@
 
     function PlayExtra_loadDataSuccessEnd(qualities) {
         UserLiveFeed_Hide();
+        Android.mSwitchPlayerAudio(Play_controlsAudioPos);
+        PlayExtra_data.watching_time = new Date().getTime();
+        Play_SetAudioIcon();
         Android.SetAuto2(PlayExtra_data.AutoUrl);
         PlayExtra_data.qualities = qualities;
         PlayExtra_SetPanel();
