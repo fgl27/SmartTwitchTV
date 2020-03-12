3955f098ce406505d72acbf3c684e158518fbfe0
fgl27 <fglfgl27@gmail.com>
2020-03-12 18:41:01 -0300

    Up publishVersionCode 142
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -123,8 +123,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.141';
-var Main_minversion = 'March 11, 2020';
+var Main_stringVersion_Min = '.142';
+var Main_minversion = 'March 12, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
 var Main_AndroidSDK = 1000;
