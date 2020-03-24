01472264a873c22aebdf346bbb12520e6d220514
fgl27 <fglfgl27@gmail.com>
2020-03-24 00:19:41 -0300

    Up publishVersionCode 151
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -123,7 +123,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.150';
+var Main_stringVersion_Min = '.151';
 var Main_minversion = 'March 23, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsNotBrowserVersion = '';
