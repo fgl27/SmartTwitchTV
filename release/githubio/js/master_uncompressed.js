508237f45cc84aae7c993542166464034d656f83
fgl27 <fglfgl27@gmail.com>
2020-03-24 23:30:23 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -3561,7 +3561,7 @@
     var Main_DataAttribute = 'data_attribute';
 
     var Main_stringVersion = '3.0';
-    var Main_stringVersion_Min = '.153';
+    var Main_stringVersion_Min = '.154';
     var Main_minversion = 'March 24, 2020';
     var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
     var Main_IsNotBrowserVersion = '';
