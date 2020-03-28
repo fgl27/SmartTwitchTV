95a02eb3943b14a265a73b5701e49f04beffae92
fgl27 <fglfgl27@gmail.com>
2020-03-28 04:13:18 -0300

    Revert "Up publishVersionCode 161"
    This reverts commit ad4995859a495365db5f080f70b51403c276c83f.

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -122,7 +122,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.161';
+var Main_stringVersion_Min = '.160';
 var Main_minversion = 'March 27, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOnAndroidVersion = '';
