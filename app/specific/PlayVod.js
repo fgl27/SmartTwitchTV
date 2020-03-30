73308e8c4eb71077bb8d14fc888755721d9cdf7d
fgl27 <fglfgl27@gmail.com>
2020-03-30 14:58:13 -0300

    Improve PlayVod_previews_success
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/PlayVod.js
+++ app/specific/PlayVod.js
@@ -988,6 +988,7 @@ var PlayVod_previews_count;
 var PlayVod_previews_rows;
 var PlayVod_previews_cols;
 var PlayVod_previews_images = [];
+var PlayVod_previews_tmp_images = [];
 
 var PlayVod_previews_width = 30;// = css div.seek_previews width
 var PlayVod_previews_height = 16.875;// = css div.seek_previews height
@@ -1029,12 +1030,10 @@ function PlayVod_previews_success(result) {
 
     Play_seek_previews.backgroundSize = (PlayVod_previews_cols * PlayVod_previews_width) + "vh";
 
-    var imgUrl, img;
-
+    PlayVod_previews_tmp_images = [];
     for (var i = 0; i < PlayVod_previews_images.length; i++) {
-        imgUrl = Main_values.Main_seek_previews_url.split(Main_values.ChannelVod_vodId)[0] + PlayVod_previews_images[i];
-        img = new Image();
-        img.src = imgUrl;
+        PlayVod_previews_tmp_images[i] = new Image();
+        PlayVod_previews_tmp_images[i].src = Main_values.Main_seek_previews_url.split(Main_values.ChannelVod_vodId)[0] + PlayVod_previews_images[i];
     }
 }
 
