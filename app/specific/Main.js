7e62cfc477c4add6956feeab3f17100aed4a2b6b
fgl27 <fglfgl27@gmail.com>
2020-04-04 15:14:47 -0300

    Up publishVersionCode 166
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -122,8 +122,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.165';
-var Main_minversion = 'April 2, 2020';
+var Main_stringVersion_Min = '.166';
+var Main_minversion = 'April 4, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOnAndroidVersion = '';
 var Main_AndroidSDK = 1000;
