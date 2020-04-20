5faa05844876cd76914bf9ad4dcfdff5da559e8e
fgl27 <fglfgl27@gmail.com>
2020-04-20 17:47:04 -0300

    Up publishVersionCode 173
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -131,7 +131,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.172';
+var Main_stringVersion_Min = '.173';
 var Main_minversion = 'April 20, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOnAndroidVersion = '';
