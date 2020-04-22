a76b2694c5e95d351e1c63461c6923662fb2b604
fgl27 <fglfgl27@gmail.com>
2020-04-22 03:07:44 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -4433,7 +4433,7 @@
     }
 
     function ChatLiveControls_SetEmojisObj() {
-        if (!AddUser_IsUserSet() && !AddUser_UsernameArray[0].access_token || emojis[0].hasOwnProperty('div')) return;
+        if (!AddUser_IsUserSet() || !AddUser_UsernameArray[0].access_token || emojis[0].hasOwnProperty('div')) return;
 
         for (var i = 0; i < emojis.length; i++) {
             emojis[i].id = i;
@@ -10865,7 +10865,7 @@
                 if (Main_values.Play_ChatForceDisable) {
                     Play_showWarningMidleDialog(STR_CHAT_DISABLE, 1500);
                     return;
-                } else if (!AddUser_UsernameArray[0].access_token) {
+                } else if (!AddUser_UserIsSet() || !AddUser_UsernameArray[0].access_token) {
                     Play_showWarningMidleDialog(STR_NOKEY_CHAT_WARN, 1500);
                     return;
                 }
