9f010bfef493c222fad8a5a6217f7a28f36a179f
fgl27 <fglfgl27@gmail.com>
2020-04-24 17:26:43 -0300

    Improve strings
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/languages/en_US.js
+++ app/languages/en_US.js
@@ -493,7 +493,7 @@ function en_USLang() {
     STR_CHAT_INDIVIDUAL_BACKGROUND_SUMMARY = "Modes are disable, enable (auto mode), Bright or Darker, In auto mode if the chat is above the stream it odd message will have a darker background accent color from the even, if the chat is not above (side by side for example) the color will be brigh";
     STR_CHAT_INDIVIDUAL_LINE = "Insert a line to separate it individual chat messages";
     STR_CHAT_LOGGING = "Logging in chat with current user";
-    STR_CHAT_LOGGING_SUMMARY = "The app will always logging to chat unless if chat is disable (player bottom controls), but this if set to NO will prevent logging using current username and sted will logging as anonymous, even if providing a authorization key";
+    STR_CHAT_LOGGING_SUMMARY = "The app will always logging to chat using current user when a authorization key is provided, unless chat is disable on player bottom controls, but if this option if set to NO it will prevent logging using current username and instead will logging as anonymous, even if providing a authorization key";
     STR_CHAT_JUST_SUB = "Subscribed with Tier";
     STR_CHAT_JUST_SUB_PRIME = "Subscribed with Prime";
     STR_GIFT_SUB_SENDER = " has gift a Tier";
