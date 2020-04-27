1a3176bb361bac79e4c943b0475a76f223fee26a
fgl27 <fglfgl27@gmail.com>
2020-04-27 04:25:44 -0300

    Up publishVersionCode 176
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -131,7 +131,7 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data_attribute';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.175';
+var Main_stringVersion_Min = '.176';
 var Main_minversion = 'April 27, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOnAndroidVersion = '';
