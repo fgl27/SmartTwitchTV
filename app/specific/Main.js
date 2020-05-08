e1250b5bd3b78798bbc4e5433b5b70b60d9decc7
fgl27 <fglfgl27@gmail.com>
2020-05-08 19:09:58 -0300

    Up publishVersionCode 184
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -131,8 +131,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data-array';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.183';
-var Main_minversion = 'May 06, 2020';
+var Main_stringVersion_Min = '.184';
+var Main_minversion = 'May 08, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOnAndroidVersion = '';
 var Main_AndroidSDK = 1000;
