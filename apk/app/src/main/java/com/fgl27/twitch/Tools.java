f181dd0a4e81e173150dba8b80d50aae219b659a
fgl27 <fglfgl27@gmail.com>
2020-05-17 17:28:14 -0300

    Improve buffer sizes
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/Tools.java
+++ apk/app/src/main/java/com/fgl27/twitch/Tools.java
@@ -603,7 +603,7 @@ public final class Tools {
         return new DefaultLoadControl.Builder()
                 .setAllocator(new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE))
                 .setBufferDurationsMs(
-                        50000, //DEFAULT_MIN_BUFFER_MS
+                        60000, //DEFAULT_MIN_BUFFER_MS
                         36000000, //DEFAULT_MAX_BUFFER_MS... technically infinity setTargetBufferBytes controls it
                         buffer, //DEFAULT_BUFFER_FOR_PLAYBACK_MS
                         buffer + 1000 //DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
@@ -753,7 +753,7 @@ public final class Tools {
             actManager.getMemoryInfo(memInfo);
         } else return 500000000;
 
-        return (int) (memInfo.totalMem / 24);
+        return (int) (memInfo.totalMem / 18);
     }
 
     public static Point ScreenSize(Display display) {
