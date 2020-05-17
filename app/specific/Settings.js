bbd2b7bc4d889eb9a361ff7243316d137250abec
fgl27 <fglfgl27@gmail.com>
2020-05-17 00:49:40 -0300

    Up key_up_timeout default value
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Settings.js
+++ app/specific/Settings.js
@@ -70,7 +70,7 @@ var Settings_value = {
             800, 900, 1000, 1100, 1200, 1300,
             1400, 1500, 1600, 1700, 1800, 1900, 2000
         ],
-        "defaultValue": 4
+        "defaultValue": 6
     },
     "live_feed_sort": {//Migrated to dialog
         "values": [
