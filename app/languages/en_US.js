d8c83eff491d6d592d4f727eb44f6f7b8471a3b2
fgl27 <fglfgl27@gmail.com>
2020-05-21 03:31:24 -0300

    Improve strings
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/languages/en_US.js
+++ app/languages/en_US.js
@@ -508,7 +508,7 @@ function en_USLang() {
     STR_GIFT_SUB_MYSTERY = " has gift the channel ";
     STR_CHAT_NICK_COLOR = "Better contrasted nick colors";
     STR_CHAT_NICK_COLOR_SUMMARY = "Instead of using the default nick color that some times can't be visible on a dark background, use a custom easy to see color";
-    STR_CHAT_CLEAR_MSG = "Clear chat, deleted or ban message";
+    STR_CHAT_CLEAR_MSG = "Clear chat, purge a user’s message";
     STR_CHAT_CLEAR_MSG_SUMMARY = "Purges chat messages from a specific user (typically after a timeout or ban)";
     STR_OPEN_HOST_SETTINGS = "Always open the host on a stream end if available";
     STR_PING_WARNING = 'Show "Ping to Twitch fail warning"';
