39ed17d61af79a70cfc4299ad2c7f854cab35ba5
fgl27 <fglfgl27@gmail.com>
2020-06-20 08:35:35 -0300

    Up main version
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -132,8 +132,8 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data-array';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.202';
-var Main_minversion = 'June 03, 2020';
+var Main_stringVersion_Min = '.203';
+var Main_minversion = 'June 20, 2020';
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_IsOn_OSInterfaceVersion = '';
 var Main_AndroidSDK = 1000;
