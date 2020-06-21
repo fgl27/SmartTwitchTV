dd653182f4e333365731566a3bfde3a3689ebb9a
fgl27 <fglfgl27@gmail.com>
2020-06-21 13:32:41 -0300

    Feed properly clear live chat and related fun when open a vod
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -1812,6 +1812,9 @@ function Play_OpenFeed(keyfun) {
 
         Play_data = JSON.parse(JSON.stringify(Play_data_base));
         Play_PreviewOffset = OSInterface_gettimepreview() / 1000;
+        Main_clearInterval(Play_ShowPanelStatusId);
+        Main_values.Play_WasPlaying = 0;
+        Play_ClearPlay(true);
 
         if (!Play_PreviewOffset) Play_PreviewOffset = UserLiveFeed_PreviewOffset;
 
