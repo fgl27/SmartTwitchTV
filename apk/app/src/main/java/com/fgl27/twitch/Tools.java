d3c328771e04676cb7820fbc4c16bee49eaf178a
fgl27 <fglfgl27@gmail.com>
2020-06-27 10:16:24 -0300

    supress not needed warning
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/Tools.java
+++ apk/app/src/main/java/com/fgl27/twitch/Tools.java
@@ -846,6 +846,7 @@ public final class Tools {
         );
     }
 
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public static boolean getBoolean(String name, boolean defaults, AppPreferences appPreferences) {
         return appPreferences.getBoolean(name, defaults);
     }
