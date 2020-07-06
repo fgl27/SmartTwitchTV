f3af66c3dc431c2195831c3b47bf041d6d93e8f4
fgl27 <fglfgl27@gmail.com>
2020-07-06 10:32:58 -0300

    UP publishVersionCode 216
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -152,9 +152,9 @@ var Main_classThumb = 'stream_thumbnail_focused';
 var Main_DataAttribute = 'data-array';
 
 var Main_stringVersion = '3.0';
-var Main_stringVersion_Min = '.215';
-var Main_version_java = 8;//Always update (+1 to current value) Main_version_java after update Main_stringVersion_Min or a major update of the apk is released
-var Main_minversion = 'July 04, 2020';
+var Main_stringVersion_Min = '.216';
+var Main_version_java = 9;//Always update (+1 to current value) Main_version_java after update Main_stringVersion_Min or a major update of the apk is released
+var Main_minversion = 'July 06, 2020';
 var Main_version_web = 10;//Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
 var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 var Main_update_show_toast = false;
