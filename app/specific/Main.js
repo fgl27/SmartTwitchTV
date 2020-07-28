b013f4f6a459dba5dcd9e8d1ac4d0420993612e1
fgl27 <fglfgl27@gmail.com>
2020-07-28 12:42:14 -0300

    Improve Main_EventVersion use
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -235,7 +235,7 @@ function Main_loadTranslations(language) {
             Main_IsOn_OSInterface = Main_IsOn_OSInterfaceVersion !== '';
 
         } catch (e) {
-            Main_IsOn_OSInterfaceVersion = '1.0.0';
+            Main_IsOn_OSInterfaceVersion = Main_stringVersion + Main_stringVersion_Min;
             Main_IsOn_OSInterface = 0;
             Main_body.style.backgroundColor = "rgba(155, 155, 155, 1)";//default rgba(0, 0, 0, 1)
             Main_isDebug = true;
@@ -928,6 +928,8 @@ function Main_checkVersion() {
         else Main_checkWebVersionRun();
 
         Main_EventVersion(Main_IsOn_OSInterfaceVersion, Main_minversion, Webviewversion, device);
+    } else {
+        Main_EventVersion(Main_IsOn_OSInterfaceVersion, Main_minversion, navigator.appVersion, navigator.platform);
     }
 
     var STR_ABOUT_CHANGELOG = "https://github.com/fgl27/SmartTwitchTV/blob/master/apk/Changelog.md";
@@ -2405,7 +2407,7 @@ function Main_EventChannel(obj) {
         });
 
     } catch (e) {
-        console.log("Main_EventVersion e " + e);
+        console.log("Main_EventChannel e " + e);
     }
 }
 
