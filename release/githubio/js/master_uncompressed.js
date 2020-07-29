f5bd8f83e9ae9b58ecf286db6b3ab97d52e9f750
fgl27 <fglfgl27@gmail.com>
2020-07-29 16:27:06 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -10510,10 +10510,13 @@
 
         try {
 
-            gtag('event', type, {
-                'name': name,
-                'lang': Languages_Selected
-            });
+            gtag(
+                'event',
+                type, {
+                    'name': name,
+                    'lang': Languages_Selected
+                }
+            );
 
         } catch (e) {
             console.log("Main_EventShowScreen e " + e);
@@ -10525,13 +10528,16 @@
 
         try {
 
-            gtag('event', type, {
-                'name': name,
-                'lang': lang ? lang.toUpperCase() : UNKNOWN,
-                'game': game ? game.replace(/,/g, ' ').replace(/:/g, '').replace(/'/g, '').replace(/-/g, '_') : UNKNOWN,
-                'screen': screen ? screen : UNKNOWN,
-                'mode': mode ? mode : 'NORMAL'
-            });
+            gtag(
+                'event',
+                type, {
+                    'name': name,
+                    'lang': lang ? lang.toUpperCase() : UNKNOWN,
+                    'game': game ? game.replace(/,/g, ' ').replace(/:/g, '').replace(/'/g, '').replace(/-/g, '_') : UNKNOWN,
+                    'screen': screen ? screen : UNKNOWN,
+                    'mode': mode ? mode : 'NORMAL'
+                }
+            );
 
         } catch (e) {
             console.log("Main_EventPlay e " + e);
@@ -10543,45 +10549,34 @@
 
         try {
 
-            gtag('event', 'screen_vac', {
-                'name': 'last'
-            });
+            //Delay the event if it is call too sone will not work
+            Main_setTimeout(
+                function() {
 
-            gtag(
-                'event',
-                'screen_apk', {
-                    'name': apk.replace(/\./g, '_')
-                }
+                    gtag(
+                        'event',
+                        'app_version', {
+                            'apk_version': apk.replace(/\./g, '_'),
+                            'web_version': web.replace(/\./g, '_'),
+                            'webview_version': webview.replace(/\./g, '_'),
+                            'device_model': device
+                        }
+                    );
+
+                    gtag(
+                        'event',
+                        'app_version_test', {
+                            'apk_version': apk,
+                            'web_version': web,
+                            'webview_version': webview,
+                            'device_model': device
+                        }
+                    );
+
+                },
+                15000
             );
 
-            gtag(
-                'event',
-                'screen_web', {
-                    'name': web.replace(/\./g, '_')
-                }
-            );
-
-            gtag(
-                'event',
-                'screen_webview', {
-                    'name': webview.replace(/\./g, '_')
-                }
-            );
-
-            gtag(
-                'event',
-                'screen_device', {
-                    'name': device
-                }
-            );
-
-            // gtag('event', 'app_version', {
-            //     'apk_version': apk.replace(/\./g, '_'),
-            //     'web_version': web.replace(/\./g, '_'),
-            //     'webview_version': webview.replace(/\./g, '_'),
-            //     'device_model': device
-            // });
-            console.log("Main_EventVersion");
         } catch (e) {
             console.log("Main_EventVersion e " + e);
         }
@@ -10601,10 +10596,13 @@
         try {
             if (!obj || !obj.type || !obj.screen) return;
 
-            gtag('event', 'channel', {
-                'type': obj.type,
-                'screen': Main_EventGetChannelScreen(obj)
-            });
+            gtag(
+                'event',
+                'channel', {
+                    'type': obj.type,
+                    'screen': Main_EventGetChannelScreen(obj)
+                }
+            );
 
         } catch (e) {
             console.log("Main_EventChannel e " + e);
