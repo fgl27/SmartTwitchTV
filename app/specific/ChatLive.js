5f0589bb7743abe24519c5618a0e5775c07f4db0
fgl27 <fglfgl27@gmail.com>
2020-09-17 23:30:09 -0300

    improve ChatLive_SendMessage warning timeout
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/ChatLive.js
+++ app/specific/ChatLive.js
@@ -1211,7 +1211,7 @@ function ChatLive_SendMessage(message, chat_number) {
 
             ChatLiveControls_showWarningDialog(
                 STR_CHAT_SEND_DELAY + STR_SPACE + time + (time > 1 ? STR_SECONDS : STR_SECOND),
-                1500
+                5000
             );
         }
 
