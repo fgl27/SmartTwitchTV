b64a4d2f742705a48a963dbb414331601e1019d5
fgl27 <fglfgl27@gmail.com>
2020-09-19 20:19:06 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -6979,7 +6979,7 @@
     var Main_stringVersion_Min = '.251';
     var Main_version_java = 37; //Always update (+1 to current value) Main_version_java after update Main_stringVersion_Min or a major update of the apk is released
     var Main_minversion = 'September 19 2020';
-    var Main_version_web = 72; //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
+    var Main_version_web = 73; //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
     var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 
     var Main_cursorYAddFocus = -1;
@@ -16439,7 +16439,7 @@
         if (!Main_A_includes_B(Play_data.qualityPlaying, 'Auto')) Play_SetHtmlQuality('stream_quality');
 
         if (!Play_StayDialogVisible()) {
-            PlayVod_RefreshProgressBarrStart(true, 1);
+            PlayVod_RefreshProgressBarrStart(true, 0);
         } else {
             PlayVod_PanelY = 2;
             Play_BottonIconsFocus();
@@ -18444,7 +18444,8 @@
         if (!Settings_Obj_default("keep_panel_info_visible")) {
 
             if (Main_IsOn_OSInterface &&
-                (Main_A_includes_B(PlayVod_qualityPlaying, 'Auto') || Main_A_includes_B(Play_data.qualityPlaying, 'Auto')) &&
+                ((Main_A_includes_B(PlayVod_qualityPlaying, 'Auto') && PlayVod_isOn) ||
+                    (Main_A_includes_B(Play_data.qualityPlaying, 'Auto') && Play_isOn)) &&
                 showVideoQuality) {
 
                 OSInterface_getVideoQuality(who_called);
