c3aed9ff9730493c257c1b1b8d3813d342f9d00e
fgl27 <fglfgl27@gmail.com>
2020-09-20 13:54:06 -0300

    Up master files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/master_uncompressed.js
+++ release/githubio/js/master_uncompressed.js
@@ -12900,12 +12900,10 @@
 
                         Play_clearHidePanel();
 
-                        if (!Play_StayDialogVisible()) {
-                            if (PlayVod_PanelY < 2) {
-                                PlayVod_PanelY--;
-                                Play_BottonIconsFocus();
-                            } else Play_BottomUpDown(1, 1);
-                        }
+                        if (PlayVod_PanelY < 2) {
+                            PlayVod_PanelY--;
+                            Play_BottonIconsFocus();
+                        } else Play_BottomUpDown(1, 1);
 
                         Play_setHidePanel();
 
@@ -12933,13 +12931,10 @@
 
                         Play_clearHidePanel();
 
-                        if (!Play_StayDialogVisible()) {
-
-                            if (PlayVod_PanelY < 2) {
-                                PlayVod_PanelY++;
-                                Play_BottonIconsFocus();
-                            } else Play_BottomUpDown(1, -1);
-                        }
+                        if (PlayVod_PanelY < 2) {
+                            PlayVod_PanelY++;
+                            Play_BottonIconsFocus();
+                        } else Play_BottomUpDown(1, -1);
 
                         Play_setHidePanel();
 
@@ -14084,7 +14079,7 @@
     function Play_BottomUpDown(PlayVodClip, adder) {
         if (Play_controls[Play_Panelcounter].updown) {
             Play_controls[Play_Panelcounter].updown(adder, PlayVodClip);
-        } else if (adder === 1) {
+        } else if (adder === 1 && (PlayVodClip || !Play_StayDialogVisible())) {
             PlayVod_PanelY--;
             Play_BottonIconsFocus();
         }
