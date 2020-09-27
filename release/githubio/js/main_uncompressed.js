2d25ac9e01aae70a4dec7ceee9aacd0e96e0b9f0
fgl27 <fglfgl27@gmail.com>
2020-09-26 23:28:09 -0300

    Up main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -6990,10 +6990,10 @@
     var Main_isDebug = false;
 
     var Main_stringVersion = '3.0';
-    var Main_stringVersion_Min = '.258';
-    var Main_version_java = 42; //Always update (+1 to current value) Main_version_java after update Main_stringVersion_Min or a major update of the apk is released
+    var Main_stringVersion_Min = '.259';
+    var Main_version_java = 43; //Always update (+1 to current value) Main_version_java after update Main_stringVersion_Min or a major update of the apk is released
     var Main_minversion = 'September 26 2020';
-    var Main_version_web = 82; //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
+    var Main_version_web = 83; //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
     var Main_versionTag = Main_stringVersion + Main_stringVersion_Min + '-' + Main_minversion;
 
     var Main_cursorYAddFocus = -1;
@@ -7578,7 +7578,7 @@
             STR_DIV_LINK + STR_ABOUT_CHANGELOG + '</div><br><br>';
 
         var changelogObj = [{
-                title: "Apk Version 3.0.258 - Web Version September 26 2020",
+                title: "Apk Version 3.0.259 - Web Version September 26 2020",
                 changes: [
                     "Update app player functionality to make it more reliable and add features, if anyone has a player issues inform, contact information on the about of the app",
                     "Open PP or Multistream mode is instantaneous now if the player preview is showing (the preview is the player that shows over the player when pressing UP)",
