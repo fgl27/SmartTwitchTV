076243df1edf8470ae7199bb3669718bb7f8db00
fgl27 <fglfgl27@gmail.com>
2020-10-13 02:27:37 -0300

    Improve Main_CheckStop
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -2043,8 +2043,6 @@ function Main_CheckStop() { // Called only by JAVA
     Main_clearInterval(PlayVod_RefreshProgressBarrID);
     Main_clearInterval(PlayVod_SaveOffsetId);
 
-    Main_clearTimeout(Play_StartStayTryId);
-
     if (PlayClip_isOn) PlayClip_Resume();
     else if (Play_isOn) {
         if (Play_MultiEnable) {
@@ -2068,6 +2066,7 @@ function Main_CheckStop() { // Called only by JAVA
         } else if (Play_data.data.length > 0 && !Play_StayDialogVisible()) Main_Set_history('live', Play_data.data);
     }
 
+    Play_StopStay();
     Main_clearTimeout(Main_setHistoryItemId);
     Main_SaveHistoryItem();
     //General related
