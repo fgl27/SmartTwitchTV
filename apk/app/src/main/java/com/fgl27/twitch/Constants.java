417dbb3644e195dfccbec90ae81cba4063e093c9
fgl27 <fglfgl27@gmail.com>
2020-11-22 11:01:33 -0300

    corrente page url
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/Constants.java
+++ apk/app/src/main/java/com/fgl27/twitch/Constants.java
@@ -24,11 +24,11 @@ public final class Constants {
 
     //Keep page url here as is easy to access and modify the value for when running test from local file
 
-    final static String PageUrl = "file:///android_asset/app/index.html";
-    final static String KeyPageUrl = "file:///android_asset/app/Extrapage/index.html";
+    //final static String PageUrl = "file:///android_asset/app/index.html";
+    //final static String KeyPageUrl = "file:///android_asset/app/Extrapage/index.html";
 
-    //final static String PageUrl = "https://fgl27.github.io/SmartTwitchTV/release/index.html";
-    //final static String KeyPageUrl = "https://fgl27.github.io/SmartTwitchTV/release/extrapageindex.html";
+    final static String PageUrl = "https://fgl27.github.io/SmartTwitchTV/release/index.html";
+    final static String KeyPageUrl = "https://fgl27.github.io/SmartTwitchTV/release/extrapageindex.html";
 
     final static String PageUrlBackup = "https://smarttv-client-for-twitch.web.app/index.html";
     final static String KeyPageUrlBackup = "https://smarttv-client-for-twitch.web.app/extrapageindex.html";
