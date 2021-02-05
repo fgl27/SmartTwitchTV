b0bd4146a62fc731314c48b305368c5c336acd36
fgl27 <fglfgl27@gmail.com>
2021-02-05 08:07:48 -0300

    Improve scripts/version.js
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/scripts/version.js
+++ release/scripts/version.js
@@ -1,5 +1,35 @@
 fs = require('fs');
-fs.writeFile('./release/githubio/version/version.json', JSON.stringify(version), function (err) {
-  if (err) return console.log(err);
-  console.log('Save version.json OK');
-});
+
+function mWriteFile(path, text, LogMsg) {
+
+    fs.writeFile(path, text, function(err) {
+        if (err) return console.log(err);
+        else if (LogMsg) console.log(LogMsg);
+    });
+
+}
+
+//UPdate version.json file use so the app can check for updates
+mWriteFile(
+    './release/githubio/version/version.json',
+    JSON.stringify(version),
+    'Save version.json OK'
+);
+
+//Update the temp Changelog_Up.md, content used to update the main Changelog.md
+var changelog = version.changelog,
+    len = changelog[0].changes.length,
+    i = 0,
+    newLine = '\n',
+    text = changelog[0].title;
+
+text += newLine + '==============' + newLine + newLine;
+
+for (i; i < len; i++) {
+    text += '* ' + changelog[0].changes[i] + newLine;
+}
+
+mWriteFile(
+    './apk/Changelog_Up.md',
+    text + newLine
+);
