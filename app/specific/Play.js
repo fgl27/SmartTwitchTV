cd431acb37062a1609861b04582bd4e88d11cd3e
fgl27 <fglfgl27@gmail.com>
2021-04-09 04:11:44 -0300

    Improve Play_SetHtmlQuality
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -932,7 +932,7 @@ function Play_ForbiddenLive() {
 function Play_loadDataSuccessFake() {
     Play_data.qualities = [
         {
-            'id': 'Auto',
+            'id': STR_AUTO,
             'band': 0,
             'codec': 'avc',
         },
@@ -1143,7 +1143,9 @@ function Play_SetHtmlQuality(element) {
 
     var quality_string = '';
 
-    if (Main_A_includes_B(Play_data.quality, 'source')) quality_string = Play_data.quality.replace("source", STR_SOURCE);
+    //Java getQualities fun will generate the first quality of the array as 'Auto'
+    if (Main_A_includes_B(Play_data.quality, 'Auto')) quality_string = Play_data.quality.replace("Auto", STR_AUTO);
+    else if (Main_A_includes_B(Play_data.quality, 'source')) quality_string = Play_data.quality.replace("source", STR_SOURCE);
     else quality_string = Play_data.quality;
 
     quality_string += !Main_A_includes_B(Play_data.quality, 'Auto') ? Play_data.qualities[Play_data.qualityIndex].band + Play_data.qualities[Play_data.qualityIndex].codec : "";
