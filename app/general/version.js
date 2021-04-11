848ad04314aded29ac90dba807c72c5e1186219a
fgl27 <fglfgl27@gmail.com>
2021-04-10 21:08:31 -0300

    Up changelog date
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/general/version.js
+++ app/general/version.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020 Felipe de Leon <fglfgl27@gmail.com>
+ * Copyright (c) 2017-2021 Felipe de Leon <fglfgl27@gmail.com>
  *
  * This file is part of SmartTwitchTV <https://github.com/fgl27/SmartTwitchTV>
  *
@@ -23,53 +23,53 @@ var version = {
     VersionBase: '3.0',
     publishVersionCode: 312,//Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
     ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/312/SmartTV_twitch_3_0_312.apk',
-    WebVersion: 'April 09 2020',
+    WebVersion: 'April 09 2021',
     WebTag: 584,//Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
     changelog: [
         {
-            title: "Apk Version 3.0.312 and Web Version April 09 2020",
+            title: "Apk Version 3.0.312 and Web Version April 09 2021",
             changes: [
                 "General improves and bug fixes"
             ]
         },
         {
-            title: "Web Version April 02 2020",
+            title: "Web Version April 02 2021",
             changes: [
                 "Fix 'Highlight @streamer messages' not working some times"
             ]
         },
         {
-            title: "Web Version March 13 2020",
+            title: "Web Version March 13 2021",
             changes: [
                 "Fix host checks"
             ]
         },
         {
-            title: "Apk Version 3.0.311 and Web Version March 10 2020",
+            title: "Apk Version 3.0.311 and Web Version March 10 2021",
             changes: [
                 "General improves and bug fixes"
             ]
         },
         {
-            title: "Web Version March 8 2020",
+            title: "Web Version March 8 2021",
             changes: [
                 "Add support to see chat messages that were sent before you joined the stream"
             ]
         },
         {
-            title: "Web Version February 28 2020",
+            title: "Web Version February 28 2021",
             changes: [
                 "General improves and bug fixes"
             ]
         },
         {
-            title: "Apk Version 3.0.310 and Web Version February 25 2020",
+            title: "Apk Version 3.0.310 and Web Version February 25 2021",
             changes: [
                 "General improves and bug fixes"
             ]
         },
         {
-            title: "Apk Version 3.0.309 and Web Version February 20 2020",
+            title: "Apk Version 3.0.309 and Web Version February 20 2021",
             changes: [
                 "General improves and bug fixes"
             ]
