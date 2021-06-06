dd0fc158865fb6ee455f30ef355e82cf23e4050f
fgl27 <fglfgl27@gmail.com>
2021-06-06 15:08:27 -0300

    Improve UserLiveFeedobj_SetLastPosition
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/UserLiveFeedobj.js
+++ app/specific/UserLiveFeedobj.js
@@ -79,7 +79,8 @@ function UserLiveFeedobj_StartDefault(pos) {
             Main_values.UserLiveFeed_LastPositionId[pos] =
                 UserLiveFeed_DataObj[pos][UserLiveFeed_FeedPosY[pos]][UserLiveFeed_FeedPosX >= UserLiveFeedobj_UserVodPos ? 7 : 14];
 
-            Main_values.UserLiveFeed_LastPosition[pos] = UserLiveFeed_FeedPosY[pos];
+            Main_values.UserLiveFeed_LastPosition[pos] = UserLiveFeed_FeedPosY[pos] < 100 ?
+                UserLiveFeed_FeedPosY[pos] : 0;
 
         }
 
@@ -1032,11 +1033,13 @@ function UserLiveFeedobj_loadDataSuccess(responseText) {
 
 function UserLiveFeedobj_SetLastPosition(pos) {
 
+    var total = UserLiveFeed_GetSize(pos);
+
     if (UserLiveFeed_idObject[pos].hasOwnProperty(Main_values.UserLiveFeed_LastPositionId[pos])) {
 
         UserLiveFeed_FeedPosY[pos] = UserLiveFeed_idObject[pos][Main_values.UserLiveFeed_LastPositionId[pos]];
 
-    } else if (UserLiveFeed_GetSize(pos)) {
+    } else if (Main_values.UserLiveFeed_LastPosition[pos] && total) {
 
         var i = Main_values.UserLiveFeed_LastPosition[pos];
 
@@ -1049,6 +1052,14 @@ function UserLiveFeedobj_SetLastPosition(pos) {
             }
 
         }
+
+    }
+
+    //There is max total loaded positions, thescroll function only works well if there is at least 4 ahead
+    //Give 5 to make things work
+    if (UserLiveFeed_obj[pos].HasMore && (UserLiveFeed_FeedPosY[pos] > (total - 5))) {
+
+        UserLiveFeed_FeedPosY[pos] = total - 5;
 
     }
 
