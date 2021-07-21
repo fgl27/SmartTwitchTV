7213558bc27499f274f1cd3e306ff269c1018e38
fgl27 <fglfgl27@gmail.com>
2021-07-21 01:05:50 -0300

    Improve release/scripts/up_assets.sh
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- apk/app/src/main/java/com/fgl27/twitch/Constants.java
+++ apk/app/src/main/java/com/fgl27/twitch/Constants.java
@@ -22,13 +22,15 @@ package com.fgl27.twitch;
 
 public final class Constants {
 
-    //Keep page url here as is easy to access and modify the value for when running test from local file
+    //Load the page from assets for testing
+    private final static boolean LoadFromAssets = false;
 
-    //final static String PageUrl = "file:///android_asset/app/index.html";
-    //final static String KeyPageUrl = "file:///android_asset/app/Extrapage/index.html";
-
-    final static String PageUrl = "https://fgl27.github.io/SmartTwitchTV/release/index.html";
-    final static String KeyPageUrl = "https://fgl27.github.io/SmartTwitchTV/release/extrapageindex.html";
+    final static String PageUrl = LoadFromAssets ?
+            "file:///android_asset/app/index.html" :
+            "https://fgl27.github.io/SmartTwitchTV/release/index.html";
+    final static String KeyPageUrl = LoadFromAssets ?
+            "file:///android_asset/app/Extrapage/index.html" :
+            "https://fgl27.github.io/SmartTwitchTV/release/extrapageindex.html";
 
     final static String PageUrlBackup = "https://smarttv-client-for-twitch.web.app/index.html";
     final static String KeyPageUrlBackup = "https://smarttv-client-for-twitch.web.app/extrapageindex.html";
--- release/scripts/up_assets.sh
+++ release/scripts/up_assets.sh
@@ -6,11 +6,19 @@ mainfolder="$(dirname "$mainfolder")";
 cd "$mainfolder" || exit
 
 if [ "$1" == 1 ]; then
+
+    sed -i 's/LoadFromAssets = true/LoadFromAssets = false/' apk/app/src/main/java/com/fgl27/twitch/Constants.java;
+
 	rm -rf apk/app/src/main/assets/app
+
 	rm -rf apk/app/src/main/assets/release
+
 	exit;
 fi;
 
+sed -i 's/LoadFromAssets = false/LoadFromAssets = true/' apk/app/src/main/java/com/fgl27/twitch/Constants.java;
+
 cp -rf app/ apk/app/src/main/assets
+
 mkdir -p apk/app/src/main/assets/release/githubio/images/
 cp -rf release/githubio/images/nordvpn/ apk/app/src/main/assets/release/githubio/images/
