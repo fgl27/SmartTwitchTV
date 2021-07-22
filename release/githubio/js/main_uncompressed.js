1d373b2cabebe11fe157f5faa9fc192628092dd8
fgl27 <fglfgl27@gmail.com>
2021-07-21 23:03:49 -0300

    Up main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -32949,8 +32949,24 @@
         else if (position === "preview_others_volume") OSInterface_SetPreviewOthersAudio(Settings_Obj_default("preview_others_volume"));
         else if (position === "preview_volume") OSInterface_SetPreviewAudio(Settings_Obj_default("preview_volume"));
         else if (position === "preview_sizes") OSInterface_SetPreviewSize(Settings_Obj_default("preview_sizes"));
-        else if (position === "show_affiliate") Main_affiliate_update();
-        else if (position === "global_font_offset") {
+        else if (position === "show_affiliate") {
+            Main_affiliate_update();
+
+            if (skipfirebase) return;
+
+            try {
+
+                gtag(
+                    'event',
+                    'banner_state', {
+                        'enable': Boolean(Settings_Obj_default("show_affiliate"))
+                    }
+                );
+
+            } catch (e) {
+                console.log("Main_EventBanner e " + e);
+            }
+        } else if (position === "global_font_offset") {
             calculateFontSize();
             AddUser_UpdateSidepanelAfterShow();
             UserLiveFeed_ResetAddCellsize();
