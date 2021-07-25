8b204b46c779542336087315576ec69dde720cae
fgl27 <fglfgl27@gmail.com>
2021-07-24 22:28:14 -0300

    Up release/scripts/http-server.sh
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/scripts/http-server.sh
+++ release/scripts/http-server.sh
@@ -8,10 +8,16 @@ gnome-terminal
 #add a alias
 #alias alias_cmd='~/workspace/smartTwitchTV/release/scripts/http-server.sh & exit'
 
+#key
+#ssh-keygen -t rsa -m PEM
+#cert
+#openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
+
 mainfolder="$(dirname ""$(dirname "$0")"")";
 mainfolder="$(dirname "$mainfolder")";
 
 cd "$mainfolder" || exit
 
 # -c-1 disable cache
-http-server -c-1
+http-server -S -C release/scripts/cert.pem -K release/scripts/key.pem -c-1
