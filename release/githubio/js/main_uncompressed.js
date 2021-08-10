b646fdd850e4b32116163e9c717e25ac43b6e194
fgl27 <fglfgl27@gmail.com>
2021-08-10 18:43:37 -0300

    Up main files
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -3853,11 +3853,17 @@
     //Spacing for release maker not trow errors from jshint
     var version = {
         VersionBase: '3.0',
-        publishVersionCode: 327, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
-        ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/327/SmartTV_twitch_3_0_327.apk',
+        publishVersionCode: 328, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
+        ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/328/SmartTV_twitch_3_0_328.apk',
         WebVersion: 'August 10 2021',
         WebTag: 603, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
         changelog: [{
+                title: "Apk Version 3.0.328",
+                changes: [
+                    "Fix minor player regression"
+                ]
+            },
+            {
                 title: "Apk Version 3.0.327 and Web Version August 10 2021",
                 changes: [
                     "Add settings Chat option to Highlight messages from the streamer and mods",
@@ -3890,28 +3896,6 @@
                     "Add a new setting Chat option 'Block bots and bot commands (!command) from show in chat'",
                     "General improves and bug fixes"
 
-                ]
-            },
-            {
-                title: "Apk Version 3.0.324 and Web Version July 17 2021",
-                changes: [
-                    "Update side panel styles",
-                    "The settings option 'Rounded channel images' is now enable by default change it in 'Customize interface, color style, animations and related'",
-                    "Add a new settings option 'Hide the side panel after 5 seconds of not using it' to 'Customize interface, color style, animations and related'",
-                    "Allow to access the 'Thumbnail Options' even when the content of the screen is empty, this is useful when changing to a language that has no content",
-                    "General improves and bug fixes"
-                ]
-            },
-            {
-                title: "Apk Version 3.0.323",
-                changes: [
-                    "General improves and bug fixes"
-                ]
-            },
-            {
-                title: "Apk Version 3.0.322",
-                changes: [
-                    "General improves and bug fixes"
                 ]
             }
         ]
