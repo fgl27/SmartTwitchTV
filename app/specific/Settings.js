dbdb505a347ff1a34214b0046fc8ce21bb99ff18
fgl27 <fglfgl27@gmail.com>
2022-01-29 10:27:16 -0300

    update Settings_DialogShowNotification
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Settings.js
+++ app/specific/Settings.js
@@ -2186,14 +2186,15 @@ function Settings_DialogShowNotification(click) {
         }
     };
 
-    if (!Main_IsOn_OSInterface || OSInterface_getSDK() < 30) {
-        obj.live_notification_background = {
-            defaultValue: Settings_value.live_notification_background.defaultValue,
-            values: Settings_value.live_notification_background.values,
-            title: STR_NOW_BACKGROUND,
-            summary: STR_NOW_BACKGROUND_SUMMARY
-        };
-    }
+    //TODO remove comments when apk is updated
+    //if (!Main_IsOn_OSInterface || OSInterface_getSDK() < 30) {
+    obj.live_notification_background = {
+        defaultValue: Settings_value.live_notification_background.defaultValue,
+        values: Settings_value.live_notification_background.values,
+        title: STR_NOW_BACKGROUND,
+        summary: STR_NOW_BACKGROUND_SUMMARY
+    };
+    // }
 
     Settings_DialogShow(obj, STR_NOTIFICATION_OPT, click);
 }
