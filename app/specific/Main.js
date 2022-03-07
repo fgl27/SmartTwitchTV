268c89cab889cf5e69861ef56483d24424746dc9
fgl27 <fglfgl27@gmail.com>
2022-03-07 10:37:11 -0300

    improve app start
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- app/specific/Main.js
+++ app/specific/Main.js
@@ -267,7 +267,7 @@ function Main_StartApp() {
 
         Main_DoRestore = AddUser_RestoreUsers();
 
-        if (!Main_values.Restore_Backup_Check && OSInterface_getSDK() < 30) {
+        if (!Main_values.Restore_Backup_Check && (!Main_IsOn_OSInterface || OSInterface_getSDK() < 30)) {
 
             try {
                 OSInterface_requestWr();
