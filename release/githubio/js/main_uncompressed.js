6f7c6cc6a3fdbfea51e8eb50cc10820a3d6bf020
fgl27 <fglfgl27@gmail.com>
2022-03-07 10:37:52 -0300

    up changes
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -11792,7 +11792,7 @@
 
             Main_DoRestore = AddUser_RestoreUsers();
 
-            if (!Main_values.Restore_Backup_Check && OSInterface_getSDK() < 30) {
+            if (!Main_values.Restore_Backup_Check && (!Main_IsOn_OSInterface || OSInterface_getSDK() < 30)) {
 
                 try {
                     OSInterface_requestWr();
