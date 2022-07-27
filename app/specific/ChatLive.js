e1917f63d46acb834c53e0b26e295f7ba990f1e9
Felipe Leon <fglfgl27@gmail.com>
2022-07-27 10:51:19 -0300

    prevent chat crash when no user is set

--- app/specific/ChatLive.js
+++ app/specific/ChatLive.js
@@ -129,10 +129,12 @@ function ChatLive_Init(chat_number, SkipClear) {
         ChatLive_loadChatters(chat_number, Chat_Id[chat_number]);
     }
 
-    ChatLive_loadGloabalEmotes(chat_number, Chat_Id[chat_number]);
+    if (ChatLive_User_Set) {
+        ChatLive_loadGloabalEmotes(chat_number, Chat_Id[chat_number]);
 
-    ChatLive_checkFallow(chat_number, Chat_Id[chat_number]);
-    ChatLive_checkSub(chat_number, Chat_Id[chat_number]);
+        ChatLive_checkFallow(chat_number, Chat_Id[chat_number]);
+        ChatLive_checkSub(chat_number, Chat_Id[chat_number]);
+    }
 }
 
 function ChatLive_Switch() {
