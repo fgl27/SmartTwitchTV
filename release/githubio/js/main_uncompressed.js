2c169f2b03c4646203751b9600b68e2520c00779
Felipe Leon <fglfgl27@gmail.com>
2022-08-14 21:07:23 -0300

    up main files

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -21645,9 +21645,7 @@
         Play_loadDataErrorFinish();
     }
 
-    function Play_loadDataSuccessEnd(playlist, startChat, SkipSaveHistory, PreventcleanQuailities) {
-        UserLiveFeed_Hide(PreventcleanQuailities);
-
+    function Play_loadDataSuccessEnd(playlist, startChat, SkipSaveHistory) {
         if (Play_EndDialogEnter === 2) PlayVod_PreshutdownStream(true);
         else if (Play_EndDialogEnter === 3) {
             PlayClip_PreshutdownStream(false);
