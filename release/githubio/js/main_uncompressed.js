429978dfac6f6097481963f52d10f306c7882151
Felipe Leon <fglfgl27@gmail.com>
2022-09-06 14:39:00 -0300

    up main files

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -4518,9 +4518,13 @@
         VersionBase: '3.0',
         publishVersionCode: 343, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
         ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/343/SmartTV_twitch_3_0_343.apk',
-        WebVersion: 'August 28 2022',
-        WebTag: 631, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
+        WebVersion: 'September 06 2022',
+        WebTag: 633, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
         changelog: [{
+                title: 'Web Version September 06 2022',
+                changes: ['Fix VOD playback for devices running Old Android version']
+            },
+            {
                 title: 'Web Version August 28 2022',
                 changes: ['Re enable Clip screen, temporarily as this API may stop work at any moment', 'General improves']
             },
@@ -4546,14 +4550,6 @@
                     'Migrate Vod seek preview image to new Twitch API',
                     'General improves'
                 ]
-            },
-            {
-                title: 'Web Version July 27 2022',
-                changes: ['Fixes issue when using the app without a user']
-            },
-            {
-                title: 'Web Version July 16 2022 and Apk Version 3.0.335 and up',
-                changes: ['Update app Player to latest version', 'General improves']
             }
         ]
     };
