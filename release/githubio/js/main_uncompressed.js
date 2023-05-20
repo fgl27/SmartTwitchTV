5761af357bc4c51753f2dfceebbbb72c893168a7
Felipe Leon <fglfgl27@gmail.com>
2023-05-19 23:05:45 -0300

    up main ifles

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -23464,11 +23464,12 @@
         Main_innerHTMLWithEle(Play_dialog_warning_play_middle_text, text);
         Play_dialog_warning_play_middle.style.width = '';
 
-        if (UserLiveFeed_isPreviewShowing()) Play_dialog_warning_play_middle.style.top = '90vh';
-        else if (IsLiveWarning) {
+        if (UserLiveFeed_isPreviewShowing()) {
+            Play_dialog_warning_play_middle.style.top = '90vh';
+        } else if (IsLiveWarning) {
             Play_dialog_warning_play_middle.style.top = '81.5vh';
             if (PlayClip_isOn) {
-                Play_dialog_warning_play_middle.style.width = PlayClip_HasVOD ? '49%' : '55%';
+                Play_dialog_warning_play_middle.style.width = PlayClip_HasVOD ? '44%' : '50%';
             } else {
                 Play_dialog_warning_play_middle.style.width = '55%';
             }
