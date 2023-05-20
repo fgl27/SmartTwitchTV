f98f3df086e5b83f20a3bc26a585d049ed0759b9
Felipe Leon <fglfgl27@gmail.com>
2023-05-19 23:07:37 -0300

    improve now live dialog position

--- app/specific/Play.js
+++ app/specific/Play.js
@@ -1284,7 +1284,7 @@ function Play_showWarningMiddleDialog(text, timeout, IsLiveWarning) {
         if (PlayClip_isOn) {
             Play_dialog_warning_play_middle.style.width = PlayClip_HasVOD ? '44%' : '50%';
         } else {
-            Play_dialog_warning_play_middle.style.width = '55%';
+            Play_dialog_warning_play_middle.style.width = '49%';
         }
     } else Play_dialog_warning_play_middle.style.top = '50vh';
 
