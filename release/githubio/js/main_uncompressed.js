9576cfbd75ba76c6c91ee21f225dc12cb0f119c9
Felipe Leon <fglfgl27@gmail.com>
2023-08-14 23:11:53 -0300

    up main files

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -32889,7 +32889,7 @@
                 return;
             }
             var id_cell = cell.stream.broadcaster.id;
-            var isNotBlocked = Screens_isNotBlocked(id_cell, cell.stream.game.id, this.IsUser);
+            var isNotBlocked = Screens_isNotBlocked(id_cell, cell.stream.game ? cell.stream.game.id : null, this.IsUser);
 
             if (!this.idObject[id_cell] && isNotBlocked) {
                 this.itemsCount++;
