2e63d8d6f5fd34c574bab87fbeb32fb6a0114828
Felipe Leon <fglfgl27@gmail.com>
2024-04-04 22:00:57 -0300

    Improve Screens_OpenGameEnd

--- app/specific/Screens.js
+++ app/specific/Screens.js
@@ -3158,8 +3158,9 @@ function Screens_ThumbOptionDialogHide(Update, key) {
                 key
             );
         } else if (!Screens_ThumbOptionPosY) Screens_OpenChannel(key);
-        else if (Screens_ThumbOptionPosY === 1) Screens_OpenGame();
-        else if (Screens_ThumbOptionPosY === 3) {
+        else if (Screens_ThumbOptionPosY === 1) {
+            Screens_OpenGame();
+        } else if (Screens_ThumbOptionPosY === 3) {
             Screens_BlockChannel(key);
         } else if (Screens_ThumbOptionPosY === 4) {
             Screens_BlockGame(key);
@@ -3683,6 +3684,8 @@ function Screens_OpenGameEnd() {
 
     Main_ExitCurrent(Main_values.Main_Go);
     Main_values.Main_Go = Main_aGame;
+
+    ScreenObj[Main_values.Main_Go].BeforeAgame = Main_values.Main_BeforeAgame === Main_Blocked ? null : Main_values.Main_BeforeAgame;
 
     Main_ReStartScreens();
 }
