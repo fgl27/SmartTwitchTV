2fd511046dbb336253b82f146a53b20a64e325ae
Felipe Leon <fglfgl27@gmail.com>
2024-04-27 00:03:43 -0300

    Improve Screens_OpenGameEnd

--- app/specific/Screens.js
+++ app/specific/Screens.js
@@ -3685,13 +3685,15 @@ function Screens_OpenGameEnd() {
             Main_values.Main_Go !== Main_ChannelClip
                 ? Main_values.Main_BeforeChannel
                 : Main_values.Main_Go;
+
         Main_values.Main_BeforeAgameisSet = true;
     }
 
     Main_ExitCurrent(Main_values.Main_Go);
-    Main_values.Main_Go = Main_aGame;
 
-    ScreenObj[Main_values.Main_Go].BeforeAgame = Main_values.Main_BeforeAgame === Main_Blocked ? null : Main_values.Main_BeforeAgame;
+    ScreenObj[Main_aGame].BeforeAgame = Main_values.Main_Go;
+
+    Main_values.Main_Go = Main_aGame;
 
     Main_ReStartScreens();
 }
