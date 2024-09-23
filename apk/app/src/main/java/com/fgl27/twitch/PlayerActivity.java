11ad55dcd0696e5ffe8b7956e254d093d80ab1a0
Felipe Leon <fglfgl27@gmail.com>
2024-09-23 17:27:55 -0300

    Revert "Comment out Crashlytics"
    This reverts commit b421b6a504a922da1e0bfd53a29610801c84ff29.

--- apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
+++ apk/app/src/main/java/com/fgl27/twitch/PlayerActivity.java
@@ -89,7 +89,7 @@ import androidx.media3.ui.PlayerView;
 import com.fgl27.twitch.channels.ChannelsUtils;
 import com.fgl27.twitch.notification.NotificationUtils;
 import com.google.firebase.FirebaseApp;
-//import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.google.gson.Gson;
 
 import net.grandcentrix.tray.AppPreferences;
@@ -339,8 +339,8 @@ public class PlayerActivity extends Activity {
             intent.setAction(null);
             setIntent(intent);
 
-            //FirebaseApp.initializeApp(this);
-            //FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
+            FirebaseApp.initializeApp(this);
+            FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
 
             try {
                 setContentView(R.layout.activity_player);
@@ -434,7 +434,7 @@ public class PlayerActivity extends Activity {
             initializeWebview();
 
             StopNotificationService();
-            //FirebaseCrashlytics.getInstance().sendUnsentReports();
+            FirebaseCrashlytics.getInstance().sendUnsentReports();
 
             DataThreadPool.execute(() -> Tools.GetUpdateFile(getApplicationContext()));
 
