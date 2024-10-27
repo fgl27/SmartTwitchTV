ffeba135b62b4d34addd4566ebc9efdc34ed5448
Felipe Leon <fglfgl27@gmail.com>
2024-10-27 10:55:56 -0300

    improve load more forced

--- app/specific/Screens.js
+++ app/specific/Screens.js
@@ -597,7 +597,12 @@ function Screens_loadDataSuccess(key) {
     ScreenObj[key].emptyContent = !ScreenObj[key].itemsCount && !ScreenObj[key].status;
 
     //TODO  improve this it may cause loops in case API bugs out
-    if ((ScreenObj[key].emptyContent || ScreenObj[key].row_id - currentRowId < 2) && response_items && !ScreenObj[key].dataEnded) {
+    if (
+        (ScreenObj[key].emptyContent || ScreenObj[key].row_id - currentRowId < 2) &&
+        ScreenObj[key].skippedCellLoadMore &&
+        response_items &&
+        !ScreenObj[key].dataEnded
+    ) {
         ScreenObj[key].loadingData = true;
         ScreenObj[key].emptyContent = false;
 
