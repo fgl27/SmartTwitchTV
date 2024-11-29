18542896a4b9b1839ce1eb2df23c057b835e1977
Felipe Leon <fglfgl27@gmail.com>
2024-11-29 15:17:56 -0300

    up main files

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -4873,10 +4873,10 @@
     //Spacing for release maker not trow errors from jshint
     var version = {
         VersionBase: '3.0',
-        publishVersionCode: 373, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
+        publishVersionCode: 372, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
         ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/372/SmartTV_twitch_3_0_372.apk',
         WebVersion: 'November 29 2024',
-        WebTag: 689, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
+        WebTag: 690, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
         changelog: [
             {
                 title: 'Version November 29 2024',
