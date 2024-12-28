d13a1ea67264a1b2344b41fa09eec6901403957e
Felipe Leon <fglfgl27@gmail.com>
2024-12-28 10:39:25 -0300

    up main files

--- app/general/version.js
+++ app/general/version.js
@@ -23,7 +23,7 @@ var version = {
     VersionBase: '3.0',
     publishVersionCode: 372, //Always update (+1 to current value) Main_version_java after update publishVersionCode or a major update of the apk is released
     ApkUrl: 'https://github.com/fgl27/SmartTwitchTV/releases/download/372/SmartTV_twitch_3_0_372.apk',
-    WebVersion: 'December 24 2024',
+    WebVersion: 'December 28 2024',
     WebTag: 697, //Always update (+1 to current value) Main_version_web after update Main_minversion or a major update of the web part of the app
     changelog: [
         {
