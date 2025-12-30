a8a3b0e006e094914bf957dd9b3e638decb52a9e
Felipe Leon <fglfgl27@gmail.com>
2025-12-30 09:42:45 -0300

    Update main.js and main_uncompressed.js
    Synchronized changes between the minified and uncompressed main JavaScript files. The update includes logic and UI improvements, bug fixes, and refactoring for better maintainability and performance.

--- release/githubio/js/main_uncompressed.js
+++ release/githubio/js/main_uncompressed.js
@@ -38752,7 +38752,7 @@ https://video-weaver.sao03.hls.ttvnw.net/v1/playlist/C.m3u8 09:36:20.90
                     return;
                 }
 
-                var id_cell = cell.stream.broadcaster.id;
+                var id_cell = cell.stream.broadcaster ? cell.stream.broadcaster.id : null;
                 var id = cell.stream.id;
                 var isNotBlocked = Screens_isNotBlocked(id_cell, cell.stream.game ? cell.stream.game.id : null, this.IsUser);
 
